c9a054b99713a7fa957cf4efcea01a2b1a36faef
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:35:50 +0530

    improve

--- cpp/cmake/modules/find_library_create_target.cmake
+++ cpp/cmake/modules/find_library_create_target.cmake
@@ -23,17 +23,22 @@ macro(find_library_create_target target_name lib libtype hints)
   message(STATUS "Looking for library ${lib}")
   if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     find_library(
-      ${lib}_LIB_PATH ${lib}${TRT_DEBUG_POSTFIX}
+      ${lib}_LIB_PATH
+      ${lib}${TRT_DEBUG_POSTFIX}
       HINTS ${hints}
-      NO_DEFAULT_PATH)
+      NO_DEFAULT_PATH
+    )
   endif()
   find_library(${lib}_LIB_PATH ${lib} HINTS ${hints} NO_DEFAULT_PATH)
   find_library(${lib}_LIB_PATH ${lib})
   message(STATUS "Library that was found ${${lib}_LIB_PATH}")
   add_library(${target_name} ${libtype} IMPORTED)
   set_target_properties(
-    ${target_name} PROPERTIES IMPORTED_LOCATION ${${lib}_LIB_PATH}
-                              IMPORTED_IMPLIB ${${lib}_LIB_PATH})
+    ${target_name}
+    PROPERTIES
+      IMPORTED_LOCATION ${${lib}_LIB_PATH}
+      IMPORTED_IMPLIB ${${lib}_LIB_PATH}
+  )
   message(
     STATUS
       "=========================================================================================="
