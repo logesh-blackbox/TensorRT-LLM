c460c4b8b99fa9aebbfb68e01cee28556f22ec13
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:25 +0530

    improve

--- cpp/tensorrt_llm/runtime/tllmRuntime.h
+++ cpp/tensorrt_llm/runtime/tllmRuntime.h
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #pragma once
 
 #include "tensorrt_llm/runtime/bufferManager.h"
@@ -33,49 +18,17 @@ public:
 
     explicit TllmRuntime(void const* engineData, std::size_t engineSize, nvinfer1::ILogger& logger);
 
-    explicit TllmRuntime(nvinfer1::IHostMemory const& engineBuffer, nvinfer1::ILogger& logger)
-        : TllmRuntime{engineBuffer.data(), engineBuffer.size(), logger}
-    {
-    }
-
     explicit TllmRuntime(void const* engineData, std::size_t engineSize);
 
-    explicit TllmRuntime(nvinfer1::IHostMemory const& engineBuffer)
-        : TllmRuntime{engineBuffer.data(), engineBuffer.size()}
-    {
-    }
+    SizeType addContext(std::int32_t profileIndex) const;
 
-    SizeType getNbContexts() const
-    {
-        return static_cast<SizeType>(mContexts.size());
-    }
+    void clearContexts() const;
 
-    nvinfer1::IExecutionContext& getContext(SizeType contextIndex) const
-    {
-        return *mContexts.at(contextIndex);
-    }
-
-    SizeType getNbProfiles() const
-    {
-        return static_cast<SizeType>(mEngine->getNbOptimizationProfiles());
-    }
-
-    nvinfer1::IExecutionContext& addContext(std::int32_t profileIndex);
-
-    void clearContexts();
-
-    void setInputTensors(SizeType contextIndex, TensorMap const& tensorMap);
-
-    void setOutputTensors(SizeType contextIndex, TensorMap& tensorMap);
+    void setTensors(SizeType contextIndex, TensorMap const& tensorMap) const;
 
     bool executeContext(SizeType contextIndex) const;
 
     CudaStream const& getStream() const;
-
-    BufferManager::CudaStreamPtr getStreamPtr()
-    {
-        return mStream;
-    }
 
     nvinfer1::ICudaEngine& getEngine()
     {
@@ -103,7 +56,8 @@ private:
     std::unique_ptr<nvinfer1::IRuntime> mRuntime;
     std::unique_ptr<nvinfer1::ICudaEngine> mEngine;
     BufferManager::IBufferPtr mEngineBuffer;
-    std::vector<std::unique_ptr<nvinfer1::IExecutionContext>> mContexts;
+    mutable std::vector<std::unique_ptr<nvinfer1::IExecutionContext>> mContexts;
     std::unique_ptr<ITensor> mDummyTensor;
 };
 } // namespace tensorrt_llm::runtime
+
