3e189ac71d86e4668bcf07e7577478117bcd0809
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:30 +0530

    improve

--- examples/chatglm2-6b/build.py
+++ examples/chatglm2-6b/build.py
@@ -12,11 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import argparse
 import os
 import time
 
-import tensorrt as trt
 import torch
 import torch.multiprocessing as mp
 import transformers
@@ -38,14 +38,14 @@ MODEL_NAME = "chatglm2-6b"
 
 
 def get_engine_name(model, dtype, tp_size, rank):
-    return '{}_{}_tp{}_rank{}.engine'.format(model, dtype, tp_size, rank)
+    return f'{model}_{dtype}_tp{tp_size}_rank{rank}.engine'
 
 
 def serialize_engine(engine, path):
     logger.info(f'Serializing engine to {path}...')
     tik = time.time()
     with open(path, 'wb') as f:
-        f.write(bytearray(engine))
+        f.write(engine.serialize())
     tok = time.time()
     t = time.strftime('%H:%M:%S', time.gmtime(tok - tik))
     logger.info(f'Engine serialized. Total time: {t}')
@@ -121,279 +121,4 @@ def parse_arguments():
     parser.add_argument('--builder_opt', type=int, default=None)
     parser.add_argument(
         '--output_dir',
-        type=str,
-        default='trtModel',
-        help=
-        'The path to save the serialized engine files, timing cache file and model configs'
-    )
-    parser.add_argument(
-        "--multi_query_mode",
-        "-mq",
-        default=False,
-        action='store_true',
-        help=
-        "Whether this model uses multi-query attention mechanism (default: False)"
-    )
-    # Arguments related to the quantization of the model.
-    parser.add_argument(
-        '--use_smooth_quant',
-        default=False,
-        action="store_true",
-        help=
-        'Use the SmoothQuant method to quantize activations and weights for the various GEMMs.'
-        'See --per_channel and --per_token for finer-grained quantization options.'
-    )
-    parser.add_argument(
-        '--use_weight_only',
-        default=False,
-        action="store_true",
-        help='Quantize weights for the various GEMMs to INT4/INT8.'
-        'See --weight_only_precision to set the precision')
-    parser.add_argument(
-        '--weight_only_precision',
-        const='int8',
-        type=str,
-        nargs='?',
-        default='int8',
-        choices=['int8', 'int4'],
-        help=
-        'Define the precision for the weights when using weight-only quantization.'
-        'You must also use --use_weight_only for that argument to have an impact.'
-    )
-    parser.add_argument(
-        '--per_channel',
-        default=False,
-        action="store_true",
-        help=
-        'By default, we use a single static scaling factor for the GEMM\'s result. '
-        'per_channel instead uses a different static scaling factor for each channel. '
-        'The latter is usually more accurate, but a little slower.')
-    parser.add_argument(
-        '--per_token',
-        default=False,
-        action="store_true",
-        help=
-        'By default, we use a single static scaling factor to scale activations in the int8 range. '
-        'per_token chooses at run time, and for each token, a custom scaling factor. '
-        'The latter is usually more accurate, but a little slower.')
-    parser.add_argument(
-        '--int8_kv_cache',
-        default=False,
-        action="store_true",
-        help=
-        'By default, we use dtype for KV cache. int8_kv_cache chooses int8 quantization for KV'
-    )
-    parser.add_argument(
-        '--random_seed',
-        type=int,
-        default=None,
-        help=
-        'Seed to use when initializing the random number generator for torch.')
 
-    args = parser.parse_args()
-    assert not (
-        args.use_smooth_quant and args.use_weight_only
-    ), "You cannot enable both SmoothQuant and INT8 weight-only together."
-
-    if args.use_smooth_quant:
-        args.quant_mode = QuantMode.use_smooth_quant(args.per_token,
-                                                     args.per_channel)
-    elif args.use_weight_only:
-        args.quant_mode = QuantMode.use_weight_only(
-            args.weight_only_precision == 'int4')
-    else:
-        args.quant_mode = QuantMode(0)
-    args.bias = not args.no_bias
-
-    if args.inter_size is None:
-        args.inter_size = 4 * args.n_embd
-
-    if args.int8_kv_cache:
-        assert (
-            args.use_gpt_attention_plugin
-        ), "You have to use GPT attention plugin when int8 KV cache is set"
-        args.quant_mode = args.quant_mode.set_int8_kv_cache()
-
-    return args
-
-
-def build_rank_engine(builder: Builder,
-                      builder_config: tensorrt_llm.builder.BuilderConfig,
-                      engine_name, rank, args):
-    '''
-       @brief: Build the engine on the given rank.
-       @param rank: The rank to build the engine.
-       @param args: The cmd line arguments.
-       @return: The built engine.
-    '''
-    str_dtype_to_trt(args.dtype)
-
-    # Initialize Module
-    tensorrt_llm_ChatGLM2_6BModel = ChatGLM2HeadModel(
-        hidden_size=4096,
-        num_attention_heads=32,
-        kv_channels=128,
-        multi_query_group_num=2,
-        apply_query_key_layer_scaling=False,
-        attention_mask_type=AttentionMaskType.causal,
-        qkv_bias=True,
-        linear_bias=False,
-        use_int8_kv_cache=False,
-        mapping=Mapping(world_size=args.world_size,
-                        rank=rank,
-                        tp_size=args.world_size),
-        ffn_hiden_size=13696,
-        num_layers=28,
-        eps=1e-5,
-        act_func='swiglu',
-        dtype=trt.float16,
-        quant_mode=QuantMode(0),
-        max_seq_length=32768,
-        vocab_size=65024)
-
-    if args.use_smooth_quant:
-        tensorrt_llm_ChatGLM2_6BModel = smooth_quantize(
-            tensorrt_llm_ChatGLM2_6BModel, args.quant_mode)
-    elif args.use_weight_only:
-        tensorrt_llm_ChatGLM2_6BModel = weight_only_quantize(
-            tensorrt_llm_ChatGLM2_6BModel, args.quant_mode)
-    if args.model_dir is not None:
-        print('loading weights from hugging face model')
-        hf_model = transformers.AutoModel.from_pretrained(
-            args.model_dir, trust_remote_code=True).cpu()
-        tensorrt_llm_ChatGLM2_6BModel = load_from_hf_chatglm2_6B(
-            tensorrt_llm_ChatGLM2_6BModel, hf_model, dtype='float16')
-        print('load finished')
-        del hf_model
-    # Module -> Network
-    network = builder.create_network()
-    network.trt_network.name = engine_name
-    if args.use_gpt_attention_plugin:
-        network.plugin_config.set_gpt_attention_plugin(
-            dtype=args.use_gpt_attention_plugin)
-    if args.use_gemm_plugin:
-        network.plugin_config.set_gemm_plugin(dtype=args.use_gemm_plugin)
-    if args.use_layernorm_plugin:
-        network.plugin_config.set_layernorm_plugin(
-            dtype=args.use_layernorm_plugin)
-    assert not (args.enable_context_fmha and args.enable_context_fmha_fp32_acc)
-    if args.enable_context_fmha:
-        network.plugin_config.set_context_fmha(ContextFMHAType.enabled)
-    if args.enable_context_fmha_fp32_acc:
-        network.plugin_config.set_context_fmha(
-            ContextFMHAType.enabled_with_fp32_acc)
-
-    # Quantization plugins.
-    if args.use_smooth_quant:
-        network.plugin_config.set_smooth_quant_gemm_plugin(dtype=args.dtype)
-        network.plugin_config.set_layernorm_quantization_plugin(
-            dtype=args.dtype)
-        # FIXME
-        network.plugin_config.set_quantize_tensor_plugin()
-        network.plugin_config.set_quantize_per_token_plugin()
-    elif args.use_weight_only:
-        network.plugin_config.set_weight_only_quant_matmul_plugin(
-            dtype='float16')
-
-    if args.world_size > 1:
-        network.plugin_config.set_nccl_plugin(args.dtype)
-    with net_guard(network):
-        # Prepare
-        network.set_named_parameters(
-            tensorrt_llm_ChatGLM2_6BModel.named_parameters())
-
-        # Forward
-        inputs = tensorrt_llm_ChatGLM2_6BModel.prepare_inputs(
-            args.max_batch_size, args.max_input_len, args.max_output_len, True,
-            args.max_beam_width)
-        tensorrt_llm_ChatGLM2_6BModel(*inputs)
-
-    tensorrt_llm.graph_rewriting.optimize(network)
-
-    engine = None
-
-    # Network -> Engine
-    engine = builder.build_engine(network, builder_config)
-    if rank == 0:
-        config_path = os.path.join(args.output_dir, 'config.json')
-        builder.save_config(builder_config, config_path)
-    return engine
-
-
-def build(rank, args):
-    torch.cuda.set_device(rank % args.gpus_per_node)
-    tensorrt_llm.logger.set_level(args.log_level)
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-
-    # when doing serializing build, all ranks share one engine
-    apply_query_key_layer_scaling = False
-    builder = Builder()
-
-    cache = None
-    for cur_rank in range(args.world_size):
-        # skip other ranks if parallel_build is enabled
-        if args.parallel_build and cur_rank != rank:
-            continue
-        builder_config = builder.create_builder_config(
-            name=MODEL_NAME,
-            precision=args.dtype,
-            timing_cache=args.timing_cache if cache is None else cache,
-            tensor_parallel=args.world_size,  # TP only
-            parallel_build=args.parallel_build,
-            num_layers=args.n_layer,
-            num_heads=args.n_head,
-            hidden_size=args.n_embd,
-            vocab_size=args.vocab_size,
-            hidden_act=args.hidden_act,
-            max_position_embeddings=args.n_positions,
-            apply_query_key_layer_scaling=apply_query_key_layer_scaling,
-            max_batch_size=args.max_batch_size,
-            max_input_len=args.max_input_len,
-            max_output_len=args.max_output_len,
-            int8=(args.quant_mode.has_act_and_weight_quant()
-                  or args.quant_mode.has_int8_kv_cache()),
-            opt_level=args.builder_opt,
-            multi_query_mode=args.multi_query_mode)
-
-        engine_name = get_engine_name(MODEL_NAME, args.dtype, args.world_size,
-                                      cur_rank)
-        engine = build_rank_engine(builder, builder_config, engine_name,
-                                   cur_rank, args)
-        assert engine is not None, f'Failed to build engine for rank {cur_rank}'
-
-        if cur_rank == 0:
-            # Use in-memory timing cache for multiple builder passes.
-            if not args.parallel_build:
-                cache = builder_config.trt_builder_config.get_timing_cache()
-
-        serialize_engine(engine, os.path.join(args.output_dir, engine_name))
-
-    if rank == 0:
-        ok = builder.save_timing_cache(
-            builder_config, os.path.join(args.output_dir, "model.cache"))
-        assert ok, "Failed to save timing cache."
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
-
-    if args.random_seed is not None:
-        torch.manual_seed(args.random_seed)
-
-    logger.set_level(args.log_level)
-    tik = time.time()
-    if args.parallel_build and args.world_size > 1 and \
-            torch.cuda.device_count() >= args.world_size:
-        logger.warning(
-            f'Parallelly build TensorRT engines. Please make sure that all of the {args.world_size} GPUs are totally free.'
-        )
-        mp.spawn(build, nprocs=args.world_size, args=(args, ))
-    else:
-        args.parallel_build = False
-        logger.info('Serially build TensorRT engines.')
-        build(0, args)
-
-    tok = time.time()
-    t = time.strftime('%H:%M:%S', time.gmtime(tok - tik))
-    logger.info(f'Total time of building all {args.world_size} engines: {t}')
