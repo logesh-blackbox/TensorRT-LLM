fef3eac0269f8e02e7d68ec42a3ee126c754336c
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:22 +0530

    comment

--- tensorrt_llm/models/unet/unet_2d_condition.py
+++ tensorrt_llm/models/unet/unet_2d_condition.py
@@ -12,6 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from typing import List, Tuple
+
 from ...functional import silu
 from ...layers import Conv2d, GroupNorm
 from ...module import Module, ModuleList
@@ -21,6 +27,28 @@ from .unet_2d_blocks import (UNetMidBlock2DCrossAttn, get_down_block,
 
 
 class UNet2DConditionModel(Module):
+    """
+    A U-Net model with conditioning on timesteps.
+
+    Args:
+        sample_size (int, optional): The size of the input sample. Defaults to None.
+        in_channels (int, optional): The number of input channels. Defaults to 4.
+        out_channels (int, optional): The number of output channels. Defaults to 4.
+        center_input_sample (bool, optional): Whether to center the input sample. Defaults to False.
+        flip_sin_to_cos (bool, optional): Whether to flip sin to cos in the time embedding. Defaults to True.
+        freq_shift (int, optional): The frequency shift for the time embedding. Defaults to 0.
+        down_block_types (Tuple[str], optional): The types of down-sampling blocks. Defaults to ("CrossAttnDownBlock2D", "CrossAttnDownBlock2D", "CrossAttnDownBlock2D", "DownBlock2D").
+        up_block_types (Tuple[str], optional): The types of up-sampling blocks. Defaults to ("UpBlock2D", "CrossAttnUpBlock2D", "CrossAttnUpBlock2D", "CrossAttnUpBlock2D").
+        block_out_channels (Tuple[int], optional): The number of output channels for each block. Defaults to (320, 640, 1280, 1280).
+        layers_per_block (int, optional): The number of layers per block. Defaults to 2.
+        downsample_padding (int, optional): The padding for down-sampling. Defaults to 1.
+        mid_block_scale_factor (float, optional): The scale factor for the mid block. Defaults to 1.0.
+        act_fn (str, optional): The activation function. Defaults to "silu".
+        norm_num_groups (int, optional): The number of groups for normalization. Defaults to 32.
+        norm_eps (float, optional): The epsilon value for normalization. Defaults to 1e-5.
+        cross_attention_dim (int, optional): The dimension for cross-attention. Defaults to 1280.
+        attention_head_dim (int, optional): The dimension for attention heads. Defaults to 8.
+    """
 
     def __init__(
         self,
@@ -49,22 +77,24 @@ class UNet2DConditionModel(Module):
         self.sample_size = sample_size
         time_embed_dim = block_out_channels[0] * 4
 
-        # input
+        # Input convolution
         self.conv_in = Conv2d(in_channels,
                               block_out_channels[0],
                               kernel_size=(3, 3),
                               padding=(1, 1))
-        # time
+
+        # Time embedding
         self.time_proj = Timesteps(block_out_channels[0], flip_sin_to_cos,
                                    freq_shift)
         timestep_input_dim = block_out_channels[0]
 
         self.time_embedding = TimestepEmbedding(timestep_input_dim,
                                                 time_embed_dim)
+
         down_blocks = []
         up_blocks = []
 
-        # down
+        # Down-sampling blocks
         output_channel = block_out_channels[0]
         for i, down_block_type in enumerate(down_block_types):
             input_channel = output_channel
@@ -76,112 +106,4 @@ class UNet2DConditionModel(Module):
                 num_layers=layers_per_block,
                 in_channels=input_channel,
                 out_channels=output_channel,
-                temb_channels=time_embed_dim,
-                add_downsample=not is_final_block,
-                resnet_eps=norm_eps,
-                resnet_act_fn=act_fn,
-                cross_attention_dim=cross_attention_dim,
-                attn_num_head_channels=attention_head_dim,
-                downsample_padding=downsample_padding,
-            )
-            down_blocks.append(down_block)
-        self.down_blocks = ModuleList(down_blocks)
-        # mid
-        self.mid_block = UNetMidBlock2DCrossAttn(
-            in_channels=block_out_channels[-1],
-            temb_channels=time_embed_dim,
-            resnet_eps=norm_eps,
-            resnet_act_fn=act_fn,
-            output_scale_factor=mid_block_scale_factor,
-            resnet_time_scale_shift="default",
-            cross_attention_dim=cross_attention_dim,
-            attn_num_head_channels=attention_head_dim,
-            resnet_groups=norm_num_groups,
-        )
-        # up
-        reversed_block_out_channels = list(reversed(block_out_channels))
-        output_channel = reversed_block_out_channels[0]
-        for i, up_block_type in enumerate(up_block_types):
-            prev_output_channel = output_channel
-            output_channel = reversed_block_out_channels[i]
-            input_channel = reversed_block_out_channels[min(
-                i + 1,
-                len(block_out_channels) - 1)]
 
-            is_final_block = i == len(block_out_channels) - 1
-
-            up_block = get_up_block(
-                up_block_type,
-                num_layers=layers_per_block + 1,
-                in_channels=input_channel,
-                out_channels=output_channel,
-                prev_output_channel=prev_output_channel,
-                temb_channels=time_embed_dim,
-                add_upsample=not is_final_block,
-                resnet_eps=norm_eps,
-                resnet_act_fn=act_fn,
-                cross_attention_dim=cross_attention_dim,
-                attn_num_head_channels=attention_head_dim,
-            )
-            up_blocks.append(up_block)
-            prev_output_channel = output_channel
-        self.up_blocks = ModuleList(up_blocks)
-        # out
-        self.conv_norm_out = GroupNorm(num_channels=block_out_channels[0],
-                                       num_groups=norm_num_groups,
-                                       eps=norm_eps)
-        self.conv_act = silu
-        self.conv_out = Conv2d(block_out_channels[0],
-                               out_channels, (3, 3),
-                               padding=(1, 1))
-
-    def forward(self, sample, timesteps, encoder_hidden_states):
-        t_emb = self.time_proj(timesteps)
-        emb = self.time_embedding(t_emb)
-
-        sample = self.conv_in(sample)
-
-        down_block_res_samples = (sample, )
-        for downsample_block in self.down_blocks:
-
-            if hasattr(
-                    downsample_block,
-                    "attentions") and downsample_block.attentions is not None:
-
-                sample, res_samples = downsample_block(
-                    hidden_states=sample,
-                    temb=emb,
-                    encoder_hidden_states=encoder_hidden_states)
-            else:
-                sample, res_samples = downsample_block(hidden_states=sample,
-                                                       temb=emb)
-            down_block_res_samples += res_samples
-
-        sample = self.mid_block(sample,
-                                emb,
-                                encoder_hidden_states=encoder_hidden_states)
-
-        for upsample_block in self.up_blocks:
-
-            res_samples = down_block_res_samples[-len(upsample_block.resnets):]
-            down_block_res_samples = down_block_res_samples[:-len(upsample_block
-                                                                  .resnets)]
-
-            if hasattr(upsample_block,
-                       "attentions") and upsample_block.attentions is not None:
-                sample = upsample_block(
-                    hidden_states=sample,
-                    temb=emb,
-                    res_hidden_states_tuple=res_samples,
-                    encoder_hidden_states=encoder_hidden_states,
-                )
-            else:
-                sample = upsample_block(hidden_states=sample,
-                                        temb=emb,
-                                        res_hidden_states_tuple=res_samples)
-
-        sample = self.conv_norm_out(sample)
-        sample = self.conv_act(sample)
-        sample = self.conv_out(sample)
-
-        return sample
