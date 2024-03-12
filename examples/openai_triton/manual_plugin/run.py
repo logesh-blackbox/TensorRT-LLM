31f19c7e3a1c46065a7e4fb26088f513ef61faab
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:41 +0530

    improve

--- examples/openai_triton/manual_plugin/run.py
+++ examples/openai_triton/manual_plugin/run.py
@@ -1,18 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
 import json
 import math
@@ -133,4 +118,11 @@ def run(engine_dir,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('
+    parser.add_argument('--engine_dir', type=str, required=True,
+                        help='Path to engine directory')
+    parser.add_argument('--batch_size', type=int, default=1,
+                        help='Batch size')
+    parser.add_argument('--seq_len', type=int, default=128,
+                        help='Sequence length')
+    parser.add_argument('--num_heads', type=int, default=8,
+                        help='Number
