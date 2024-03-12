2f0a5d3cb1baa4f83f0b58cd4ac80596729e1d84
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:15 +0530

    improve

--- docker/common/install_polygraphy.sh
+++ docker/common/install_polygraphy.sh
@@ -6,10 +6,11 @@
 set -ex
 
 # Step 2: Define the URL for the TensorRT 9.0.1 Polygraphy package.
-RELEASE_URL_PG=https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/secure/9.0.1/tars/polygraphy-0.48.1-py2.py3-none-any.whl
+RELEASE_URL_PG="https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/secure/9.0.1/tars/polygraphy-0.48.1-py2.py3-none-any.whl"
 
 # Step 3: Uninstall any currently installed version of the Polygraphy package.
 pip uninstall -y polygraphy
 
 # Step 4: Install the specified Polygraphy package version using pip.
-pip install ${RELEASE_URL_PG}
+pip install "${RELEASE_URL_PG}"
+
