0cc16d278114d75710177c28f0f9eaa62a92e9be
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:52 +0530

    comment

--- cpp/tensorrt_llm/kernels/samplingPenaltyKernels.h
+++ cpp/tensorrt_llm/kernels/samplingPenaltyKernels.h
@@ -25,22 +25,42 @@ namespace tensorrt_llm
 namespace kernels
 {
 
+/**
+ * @brief Applies repetition penalty to the given logits.
+ *
+ * This function applies a repetition penalty to the given logits based on the specified penalty type.
+ *
+ * @tparam T The data type of the logits.
+ * @param logits The logits to apply the repetition penalty to.
+ * @param penalties The penalties to apply to the logits.
+ * @param output_ids The output IDs of the sequence.
+ * @param sequence_lengths The lengths of the sequences in the batch.
+ * @param batch_size The size of the batch.
+ * @param local_batch_size The local batch size.
+ * @param vocab_size The size of the vocabulary.
+ * @param input_lengths The lengths of the input sequences.
+ * @param penalty_type The type of repetition penalty to apply.
+ * @param max_seq_len The maximum sequence length.
+ * @param stream The CUDA stream to execute the operation on.
+ */
 template <typename T>
 void invokeBatchApplyRepetitionPenalty(T* logits, const float* penalties, const int** output_ids,
     const int* sequence_lengths, const int batch_size, const int local_batch_size, const int vocab_size,
     const int* input_lengths, const RepetitionPenaltyType penalty_type, int max_seq_len, cudaStream_t stream);
 
+/**
+ * @brief Applies temperature penalty to the given logits.
+ *
+ * This function applies a temperature penalty to the given logits based on the specified temperature.
+ *
+ * @tparam T The data type of the logits.
+ * @param logits The logits to apply the temperature penalty to.
+ * @param bias The bias to apply to the logits.
+ * @param temperature The temperature to apply to the logits.
+ * @param batch_size The size of the batch.
+ * @param vocab_size The size of the vocabulary.
+ * @param vocab_size_padd The padded size of the vocabulary.
+ * @param stream The CUDA stream to execute the operation on.
+ */
 template <typename T>
-void invokeApplyTemperaturePenalty(T* logits, const T* bias, const float temperature, const int batch_size,
-    const int vocab_size, const int vocab_size_padd, cudaStream_t stream);
 
-template <typename T>
-void invokeBatchApplyTemperaturePenalty(T* logits, const T* bias, const float* temperatures, const int batch_size,
-    const int vocab_size, const int vocab_size_padd, cudaStream_t stream);
-
-template <typename T>
-void invokeMinLengthPenalty(T* logits, const int* min_lengths, const int* end_ids, const int* sequnece_lengths,
-    const int* input_lengths, const int batch_size, const int vocab_size_padded, cudaStream_t stream);
-
-} // namespace kernels
-} // namespace tensorrt_llm
