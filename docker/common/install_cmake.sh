39e0f7b04030859f7f219bca32fed904e4b1f971
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:17 +0530

    improve

--- docker/common/install_cmake.sh
+++ docker/common/install_cmake.sh
@@ -5,28 +5,34 @@
 set -ex  # Exit on error and print commands.
 
 # Get the architecture of the system.
-ARCH=$(uname -m)
+architecture=$(uname -m)
 
 # Define the desired CMake version.
-CMAKE_VERSION="3.24.4"
+desired_cmake_version="3.24.4"
 
 # Extract the major and minor version from the CMAKE_VERSION variable.
-PARSED_CMAKE_VERSION=$(echo $CMAKE_VERSION | sed 's/\.[0-9]*$//')
+parsed_cmake_version="${desired_cmake_version%.*}"
 
 # Set the CMake file name based on the architecture and version.
-CMAKE_FILE_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}"
+cmake_file_name="cmake-${desired_cmake_version}-linux-${architecture}"
 
 # Define the URL for the CMake release archive.
-RELEASE_URL_CMAKE=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_FILE_NAME}.tar.gz
+release_url_cmake=https://github.com/Kitware/CMake/releases/download/v${desired_cmake_version}/${cmake_file_name}.tar.gz
+
+# Check if CMake is already installed and if the version matches.
+if command -v cmake >/dev/null 2>&1 && cmake --version | grep -q "${parsed_cmake_version}"; then
+    echo "CMake ${desired_cmake_version} is already installed."
+    exit 0
+fi
 
 # Download the CMake release archive to the /tmp directory.
-wget --no-verbose ${RELEASE_URL_CMAKE} -P /tmp
+wget --no-verbose "${release_url_cmake}" -P /tmp
 
 # Extract the CMake archive to /usr/local/.
-tar -xf /tmp/${CMAKE_FILE_NAME}.tar.gz -C /usr/local/
+tar -xf "/tmp/${cmake_file_name}.tar.gz" -C /usr/local/
 
 # Create a symbolic link to the CMake installation directory.
-ln -s /usr/local/${CMAKE_FILE_NAME} /usr/local/cmake
+ln -s "/usr/local/${cmake_file_name}" /usr/local/cmake
 
 # Add the CMake bin directory to the PATH environment variable.
-echo 'export PATH=/usr/local/cmake/bin:$PATH' >> "${BASH_ENV}"
+echo 'export PATH=/usr/local/cmake/bin:$PATH' >> "${BASH_
