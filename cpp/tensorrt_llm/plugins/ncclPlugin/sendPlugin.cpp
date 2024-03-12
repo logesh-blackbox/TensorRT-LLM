23ba694fe1fe22da480f874565761bfdf7dfd359
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:06 +0530

    comment

--- cpp/tensorrt_llm/plugins/ncclPlugin/sendPlugin.cpp
+++ cpp/tensorrt_llm/plugins/ncclPlugin/sendPlugin.cpp
@@ -14,24 +14,111 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "sendPlugin.h"
 
+// Include required TensorRT headers
+#include <cuda_runtime_api.h>
+#include <nccl.h>
+#include <plugin/plugin_common.h>
+#include <plugin/plugin_factory.h>
+#include <plugin/plugin_impl.h>
+#include <plugin/plugin_tensor_desc.h>
+#include <plugin/plugin_util.h>
+#include <vector>
+
+// Declare the namespace for the plugin
 using namespace nvinfer1;
 using tensorrt_llm::plugins::SendPluginCreator;
 using tensorrt_llm::plugins::SendPlugin;
 
+// Define the plugin version and name
 static const char* SEND_PLUGIN_VERSION{"1"};
 static const char* SEND_PLUGIN_NAME{"Send"};
+
+// Define the PluginFieldCollection and PluginField for the plugin constructor
 PluginFieldCollection SendPluginCreator::mFC{};
 std::vector<nvinfer1::PluginField> SendPluginCreator::mPluginAttributes;
 
+// Define the SendPlugin class
+class SendPlugin : public IPluginV2DynamicExt
+{
+public:
+    // Constructor for the plugin
+    SendPlugin(int tgtRank, nvinfer1::DataType type);
+
+    // Parameterized constructor for the plugin
+    SendPlugin(const void* data, size_t length);
+
+    // IPluginV2DynamicExt methods
+    nvinfer1::IPluginV2DynamicExt* clone() const noexcept override;
+    nvinfer1::DimsExprs getOutputDimensions(
+        int outputIndex, const nvinfer1::DimsExprs* inputs, int nbInputs, nvinfer1::IExprBuilder& exprBuilder) noexcept override;
+    bool supportsFormatCombination(
+        int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) noexcept override;
+    void configurePlugin(const nvinfer1::DynamicPluginTensorDesc* in, int nbInputs,
+        const nvinfer1::DynamicPluginTensorDesc* out, int nbOutputs) noexcept override;
+    size_t getWorkspaceSize(const nvinfer1::PluginTensorDesc* inputs, int nbInputs,
+        const nvinfer1::PluginTensorDesc* outputs, int nbOutputs) const noexcept override;
+    int enqueue(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
+        const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept override;
+
+    // IPluginV2Ext methods
+    nvinfer1::DataType getOutputDataType(
+        int index, const nvinfer1::DataType* inputTypes, int nbInputs) const noexcept override;
+
+    // IPluginV2 methods
+    const char* getPluginType() const noexcept override;
+    const char* getPluginVersion() const noexcept override;
+    int getNbOutputs() const noexcept override;
+    int initialize() noexcept override;
+    void terminate() noexcept override;
+    size_t getSerializationSize() const noexcept override;
+    void serialize(void* buffer) const noexcept override;
+    void destroy() noexcept override;
+
+private:
+    // Helper functions for reading and writing data
+    template <typename T>
+    void read(const char*& d, T& value);
+    template <typename T>
+    void write(char*& d, const T& value);
+
+    // Data members for the plugin
+    int mTgtRank;
+    nvinfer1::DataType mType;
+    ncclComm_t mComm;
+};
+
+// Define the SendPluginCreator class
+class SendPluginCreator : public IPluginCreator
+{
+public:
+    // Constructor for the plugin creator
+    SendPluginCreator();
+
+    // IPluginCreator methods
+    const char* getPluginName() const noexcept override;
+    const char* getPluginVersion() const noexcept override;
+    const PluginFieldCollection* getFieldNames() noexcept override;
+    IPluginV2* createPlugin(const char* name, const PluginFieldCollection* fc) noexcept override;
+    IPluginV2* deserializePlugin(const char* name, const void* serialData, size_t serialLength) noexcept override;
+
+private:
+    // Data member for the plugin namespace
+    std::string mNamespace;
+};
+
+// Implement the SendPlugin class methods
+
+// Constructor for the plugin
 SendPlugin::SendPlugin(int tgtRank, nvinfer1::DataType type)
     : mTgtRank(tgtRank)
     , mType(type)
 {
 }
 
-// Parameterized constructor
+// Parameterized constructor for the plugin
 SendPlugin::SendPlugin(const void* data, size_t length)
 {
     const char *d = reinterpret_cast<const char*>(data), *a = d;
@@ -40,7 +127,7 @@ SendPlugin::SendPlugin(const void* data, size_t length)
     TLLM_CHECK(d == a + length);
 }
 
-// IPluginV2DynamicExt Methods
+// IPluginV2DynamicExt methods
 nvinfer1::IPluginV2DynamicExt* SendPlugin::clone() const noexcept
 {
     auto* plugin = new SendPlugin(*this);
@@ -49,190 +136,4 @@ nvinfer1::IPluginV2DynamicExt* SendPlugin::clone() const noexcept
 }
 
 nvinfer1::DimsExprs SendPlugin::getOutputDimensions(
-    int outputIndex, const nvinfer1::DimsExprs* inputs, int nbInputs, nvinfer1::IExprBuilder& exprBuilder) noexcept
-{
-    return inputs[0];
-}
-
-bool SendPlugin::supportsFormatCombination(
-    int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) noexcept
-{
-    return (inOut[pos].type == mType) && (inOut[pos].format == TensorFormat::kLINEAR);
-}
-
-void SendPlugin::configurePlugin(const nvinfer1::DynamicPluginTensorDesc* in, int nbInputs,
-    const nvinfer1::DynamicPluginTensorDesc* out, int nbOutputs) noexcept
-{
-}
-
-size_t SendPlugin::getWorkspaceSize(const nvinfer1::PluginTensorDesc* inputs, int nbInputs,
-    const nvinfer1::PluginTensorDesc* outputs, int nbOutputs) const noexcept
-{
-    return 0;
-}
-
-int SendPlugin::enqueue(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
-    const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept
-{
-    if (isBuilding())
-    {
-        return 0;
-    }
-    int size = 1;
-    for (int i = 0; i < inputDesc[0].dims.nbDims; ++i)
-    {
-        size *= inputDesc[0].dims.d[i];
-    }
-
-    NCCLCHECK(ncclSend(inputs[0], size, (*getDtypeMap())[inputDesc[0].type], 1, mComm, stream));
-    return 0;
-}
-
-// IPluginV2Ext Methods
-nvinfer1::DataType SendPlugin::getOutputDataType(
-    int index, const nvinfer1::DataType* inputTypes, int nbInputs) const noexcept
-{
-    assert(index == 0);
-    return inputTypes[0];
-}
-
-// IPluginV2 Methods
-
-const char* SendPlugin::getPluginType() const noexcept
-{
-    return SEND_PLUGIN_NAME;
-}
-
-const char* SendPlugin::getPluginVersion() const noexcept
-{
-    return SEND_PLUGIN_VERSION;
-}
-
-int SendPlugin::getNbOutputs() const noexcept
-{
-    return 1;
-}
-
-int SendPlugin::initialize() noexcept
-{
-    if (isBuilding())
-    {
-        return 0;
-    }
-    int myRank, nRanks;
-    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &myRank));
-    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &nRanks));
-
-    ncclUniqueId id;
-    ncclGetUniqueId(&id);
-    MPICHECK(MPI_Send(&id, sizeof(id), MPI_BYTE, mTgtRank, 0, MPI_COMM_WORLD));
-    NCCLCHECK(ncclCommInitRank(&mComm, 2, id, 0));
-    return 0;
-}
-
-void SendPlugin::terminate() noexcept
-{
-    if (isBuilding())
-    {
-        return;
-    }
-    NCCLCHECK(ncclCommDestroy(mComm));
-}
-
-size_t SendPlugin::getSerializationSize() const noexcept
-{
-    return sizeof(mTgtRank) + sizeof(mType);
-}
-
-void SendPlugin::serialize(void* buffer) const noexcept
-{
-    char *d = static_cast<char*>(buffer), *a = d;
-    write(d, mType);
-    write(d, mTgtRank);
-    assert(d == a + getSerializationSize());
-}
-
-void SendPlugin::destroy() noexcept
-{
-    // This gets called when the network containing plugin is destroyed
-    delete this;
-}
-
-///////////////
-
-SendPluginCreator::SendPluginCreator()
-{
-    // Fill PluginFieldCollection with PluginField arguments metadata
-    mPluginAttributes.clear();
-    mPluginAttributes.emplace_back(PluginField("tgt_rank", nullptr, PluginFieldType::kINT32, 1));
-    mPluginAttributes.emplace_back(PluginField("type_id", nullptr, PluginFieldType::kINT32, 1));
-
-    mFC.nbFields = mPluginAttributes.size();
-    mFC.fields = mPluginAttributes.data();
-}
-
-const char* SendPluginCreator::getPluginName() const noexcept
-{
-    return SEND_PLUGIN_NAME;
-}
-
-const char* SendPluginCreator::getPluginVersion() const noexcept
-{
-    return SEND_PLUGIN_VERSION;
-}
-
-const PluginFieldCollection* SendPluginCreator::getFieldNames() noexcept
-{
-    return &mFC;
-}
-
-IPluginV2* SendPluginCreator::createPlugin(const char* name, const PluginFieldCollection* fc) noexcept
-{
-    const PluginField* fields = fc->fields;
-    int tgtRank;
-    nvinfer1::DataType type;
-    // Read configurations from each fields
-    for (int i = 0; i < fc->nbFields; ++i)
-    {
-        const char* attrName = fields[i].name;
-        if (!strcmp(attrName, "tgt_rank"))
-        {
-            TLLM_CHECK(fields[i].type == PluginFieldType::kINT32);
-            tgtRank = static_cast<int>(*(static_cast<const int*>(fields[i].data)));
-        }
-        else if (!strcmp(attrName, "type_id"))
-        {
-            TLLM_CHECK(fields[i].type == PluginFieldType::kINT32);
-            type = static_cast<nvinfer1::DataType>(*(static_cast<const nvinfer1::DataType*>(fields[i].data)));
-        }
-    }
-
-    try
-    {
-        auto* obj = new SendPlugin(tgtRank, type);
-        obj->setPluginNamespace(mNamespace.c_str());
-        return obj;
-    }
-    catch (const std::exception& e)
-    {
-        caughtError(e);
-    }
-    return nullptr;
-}
-
-IPluginV2* SendPluginCreator::deserializePlugin(const char* name, const void* serialData, size_t serialLength) noexcept
-{
-    // This object will be deleted when the network is destroyed, which will
-    // call SendPlugin::destroy()
-    try
-    {
-        auto* obj = new SendPlugin(serialData, serialLength);
-        obj->setPluginNamespace(mNamespace.c_str());
-        return obj;
-    }
-    catch (const std::exception& e)
-    {
-        caughtError(e);
-    }
-    return nullptr;
-}
+    int outputIndex, const
