2740227181a554601ee4c48dbfaf432249893283
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:33 +0530

    comment

--- examples/gpt/utils/nemo.py
+++ examples/gpt/utils/nemo.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import functools
 import logging
 import os
@@ -23,10 +24,12 @@ import yaml
 from transformers import GPT2Config
 from utils.convert import cpu_map_location, gpu_map_location
 
+# Initialize the logger for this module
 LOGGER = logging.getLogger(__name__)
 
-
+# Define a function to convert Nemo model configuration to GPT2Config
 def nemo_to_gpt_config(nemo_model_config, vocab_size, eos_id, bos_id):
+    # Define a dictionary to map Nemo configuration keys to GPT2Config keys
     convertion_dict = {
         "activation_function": "activation",
         "layer_norm_epsilon": "layernorm_epsilon",
@@ -39,72 +42,66 @@ def nemo_to_gpt_config(nemo_model_config, vocab_size, eos_id, bos_id):
         "intermediate_size": "ffn_hidden_size",
     }
 
-    kwargs = {
-        key: nemo_model_config[value]
-        for key, value in convertion_dict.items() if value in nemo_model_config
-    }
+    # Initialize an empty dictionary to store the GPT2Config arguments
+    kwargs = {}
+
+    # Iterate over the Nemo configuration keys and values
+    for key, value in convertion_dict.items():
+        # If the Nemo configuration key is present, add it to the GPT2Config arguments
+        if value in nemo_model_config:
+            kwargs[key] = nemo_model_config[value]
+
+    # Add the vocab_size, eos_token_id, and bos_token_id arguments to the GPT2Config
     kwargs["vocab_size"] = vocab_size
     kwargs["eos_token_id"] = eos_id
     kwargs["bos_token_id"] = bos_id
 
+    # Return a new GPT2Config instance with the specified arguments
     return GPT2Config(**kwargs)
 
 
+# Define a function to add special tokens to a tokenizer
 def add_special_tokens_to_tokenizer(tokenizer):
 
-    # Need to add cls, sep, mask tokens to the tokenizer if they don't exist.
-    # If cls, sep and mask are not attributes of the tokenizer, add it.
-    if not hasattr(tokenizer, 'cls_token'):
-        tokenizer.add_special_tokens({'cls_token': '<cls>'})
-    if not hasattr(tokenizer.tokenizer, 'sep_id'):
-        tokenizer.add_special_tokens({'sep_token': '<sep>'})
-    if not hasattr(tokenizer.tokenizer, 'mask_id'):
-        tokenizer.add_special_tokens({'mask_token': '<mask>'})
+    # Define a dictionary to map special token names to their corresponding values
+    special_tokens = {
+        "cls_token": "<cls>",
+        "sep_id": "<sep>",
+        "mask_id": "<mask>",
+        "pad_token": "<pad>",
+        "bos_token": "<bos>",
+        "eos_token": "<eos>",
+    }
 
-    # bos, eos, pad and unk may be present in the provided spm .model file, if they are, use it.
-    if not hasattr(tokenizer, 'pad_token'):
-        if hasattr(tokenizer.tokenizer,
-                   'pad_id') and tokenizer.tokenizer.pad_id() > 0:
-            tokenizer.pad_token = tokenizer.tokenizer.id_to_piece(
-                tokenizer.tokenizer.pad_id())
-        else:
-            tokenizer.add_special_tokens({'pad_token': '<pad>'})
-    else:
-        tokenizer.add_special_tokens({'pad_token': '<pad>'})
+    # Iterate over the special tokens
+    for token_name, token_value in special_tokens.items():
+        # If the token is not already present in the tokenizer, add it
+        if not hasattr(tokenizer, token_name):
+            tokenizer.add_special_tokens({token_name: token_value})
 
-    if not hasattr(tokenizer, 'bos_token'):
-        if hasattr(tokenizer.tokenizer,
-                   'bos_id') and tokenizer.tokenizer.bos_id() > 0:
-            tokenizer.bos_token = tokenizer.tokenizer.id_to_piece(
-                tokenizer.tokenizer.bos_id())
-        else:
-            tokenizer.add_special_tokens({'bos_token': '<bos>'})
-    else:
-        tokenizer.add_special_tokens({'bos_token': '<s>'})
-
-    if not hasattr(tokenizer, 'eos_token'):
-        if hasattr(tokenizer.tokenizer,
-                   'eos_id') and tokenizer.tokenizer.eos_id() > 0:
-            tokenizer.eos_token = tokenizer.tokenizer.id_to_piece(
-                tokenizer.tokenizer.eos_id())
-        else:
-            tokenizer.add_special_tokens({'eos_token': '<eos>'})
-    else:
-        tokenizer.add_special_tokens({'eos_token': '</s>'})
+        # If the token ID is not already present in the tokenizer, add it
+        if not hasattr(tokenizer.tokenizer, f"{token_name}_id"):
+            tokenizer.add_special_tokens({token_name: token_value})
 
 
-def unpack_nemo_ckpt(
-    nemo_archive_path: typing.Union[str, pathlib.Path],
-    out_dir_path: typing.Union[str, pathlib.Path],
-):
+# Define a function to unpack a Nemo checkpoint archive
+def unpack_nemo_ckpt(nemo_archive_path: typing.Union[str, pathlib.Path],
+                     out_dir_path: typing.Union[str, pathlib.Path]):
+    # Convert the input paths to pathlib.Path objects
     nemo_archive_path = pathlib.Path(nemo_archive_path)
+    out_dir_path = pathlib.Path(out_dir_path)
+
+    # Check if the archive exists
     if not nemo_archive_path.exists():
         raise FileNotFoundError(f"{nemo_archive_path} does not exist")
 
+    # Try to extract the archive using different modes
     for tar_mode in ["r:", "r:gz"]:
         try:
+            # Open the archive in read mode
             with tarfile.open(nemo_archive_path, mode=tar_mode) as tar_file:
 
+                # Define a function to check if a file path is within a directory
                 def is_within_directory(directory, target):
 
                     abs_directory = os.path.abspath(directory)
@@ -114,6 +111,7 @@ def unpack_nemo_ckpt(
 
                     return prefix == abs_directory
 
+                # Define a function to get safe members from the archive
                 def safe_members(tar_file):
                     members = []
                     for member in tar_file.getmembers():
@@ -124,170 +122,18 @@ def unpack_nemo_ckpt(
                         members.append(member)
                     return members
 
+                # Extract the archive to the output directory
                 tar_file.extractall(out_dir_path,
                                     members=safe_members(tar_file),
                                     numeric_owner=False)
 
+            # Return the output directory path
             return out_dir_path
         except tarfile.ReadError:
             pass
 
+    # Raise an error if the archive could not be extracted
     raise RuntimeError(f"Could not unpack {nemo_archive_path}")
 
 
-def extract_layers_with_prefix(model_, prefix):
-    length_to_trim = len(prefix)
-    model_state = model_.get("state_dict", model_)
-    return {
-        key[length_to_trim:]: model_state[key]
-        for key in model_state.keys() if prefix in key
-    }
-
-
-class UnpackedNemoCheckpointDir:
-
-    def __init__(self,
-                 checkpoints_dir: typing.Union[str, pathlib.Path],
-                 load_checkpoints_to_cpu: bool = False):
-        self._checkpoints_dir = pathlib.Path(checkpoints_dir)
-        self._load_checkpoints_to_cpu = load_checkpoints_to_cpu
-
-    @property
-    @functools.lru_cache
-    def model_config(self):
-        model_config = None
-
-        model_config_filename = "model_config.yaml"
-        model_configs_paths = list(
-            self._checkpoints_dir.rglob(model_config_filename))
-        if model_configs_paths:
-            if len(model_configs_paths) > 1:
-                raise RuntimeError(
-                    f"There are more than single {model_config_filename} "
-                    f"in {self._checkpoints_dir}: {', '.join(map(lambda p: p.as_posix(), model_configs_paths))}"
-                )
-            model_config_path = model_configs_paths[0]
-            LOGGER.debug("Loading model config from %s", model_config_path)
-            with model_config_path.open("r") as model_config_file:
-                model_config = yaml.load(model_config_file,
-                                         Loader=yaml.SafeLoader)
-        else:
-            LOGGER.debug("Searching model config in checkpoints")
-            # try to obtain from checkpoint
-            checkpoint_name = self.checkpoint_name
-            checkpoints_paths = sorted(
-                self._checkpoints_dir.rglob(checkpoint_name))
-            if checkpoints_paths:
-                # assume that parallel ranks 0 checkpoint should have model config embedded
-                checkpoint_path = checkpoints_paths[0]
-
-                map_location_fn = cpu_map_location if self._load_checkpoints_to_cpu else gpu_map_location
-
-                model_00 = torch.load(checkpoint_path,
-                                      map_location=map_location_fn)
-                if "hyper_parameters" in model_00 and "cfg" in model_00[
-                        "hyper_parameters"]:
-                    model_config = model_00["hyper_parameters"]["cfg"]
-                    LOGGER.debug("Loaded model config from checkpoint %s",
-                                 checkpoint_path)
-                else:
-                    LOGGER.debug("Could not find model config in checkpoint %s",
-                                 checkpoint_path)
-
-                del model_00
-
-        if model_config is None:
-            LOGGER.warning(
-                "Could not find checkpoint with NeMo model config in %s",
-                self._checkpoints_dir)
-
-        LOGGER.debug("Loaded model config %s", model_config)
-
-        return model_config
-
-    @property
-    def checkpoints_dir(self):
-        return self._checkpoints_dir
-
-    def get_checkpoints_paths(self,
-                              tensor_model_parallel_size=1,
-                              pipeline_model_parallel_size=1):
-        """
-        Injects tensor/pipeline model parallel ranks into the filepath.
-        Does nothing if not using model parallelism.
-        """
-
-        checkpoint_path_without_rank = self.checkpoints_dir / self.checkpoint_name
-
-        def _inject_parallel_ranks(tp_rank, pp_rank):
-            if tensor_model_parallel_size > 1 or pipeline_model_parallel_size > 1:
-                if pipeline_model_parallel_size is None or pipeline_model_parallel_size == 1:
-                    checkpoint_path = (checkpoint_path_without_rank.parent /
-                                       f"mp_rank_{tp_rank:02d}" /
-                                       checkpoint_path_without_rank.name)
-                else:
-                    checkpoint_path = (
-                        checkpoint_path_without_rank.parent /
-                        f"tp_rank_{tp_rank:02d}_pp_rank_{pp_rank:03d}" /
-                        checkpoint_path_without_rank.name)
-                return checkpoint_path
-            else:
-                return checkpoint_path_without_rank
-
-        return [[
-            _inject_parallel_ranks(tp_rank=tp_rank, pp_rank=pp_rank)
-            for pp_rank in range(pipeline_model_parallel_size)
-        ] for tp_rank in range(tensor_model_parallel_size)]
-
-    @property
-    @functools.lru_cache
-    def checkpoint_name(self):
-        patterns = [
-            "model_weights.ckpt",  # older megatron checkpoints
-            "*last.ckpt",  # newer format of checkpoints
-        ]
-        for pattern in patterns:
-            model_files = sorted(list(self._checkpoints_dir.rglob(pattern)))
-            if model_files:
-                return model_files[0].name
-
-        raise ValueError(
-            f"Could not find checkpoint files in {self._checkpoints_dir}")
-
-    @functools.lru_cache
-    def get_tokenizer_file_path(self, tokenizer_key, file_key,
-                                default_filename_pattern):
-        model_config = self.model_config
-        file_property = None
-        if tokenizer_key in model_config and file_key in model_config[
-                tokenizer_key]:
-            file_property = model_config[tokenizer_key][file_key]
-        elif file_key in model_config:
-            file_property = model_config[file_key]
-
-        LOGGER.debug("model_config[%s][%s]=%s", tokenizer_key, file_key,
-                     file_property)
-
-        if file_property and file_property.startswith("nemo:"):
-            filename = file_property.split("nemo:")[1]
-            filename_pattern = f"*{filename}"
-        elif file_property and file_property.startswith("/artifacts/"):
-            filename = pathlib.Path(file_property).name
-            filename_pattern = f"*{filename}"
-        elif file_property is None or file_property == "None":
-            filename_pattern = None
-        else:
-            filename_pattern = default_filename_pattern
-            LOGGER.warning(
-                f"Tokenizer file from config: {tokenizer_key}.{file_key}={file_property} "
-                f"looks like unsupported path. Pattern {filename_pattern} will be used."
-            )
-
-        file_path = None
-        if filename_pattern is not None:
-            files_paths = list(self._checkpoints_dir.glob(filename_pattern))
-            if files_paths:
-                assert len(files_paths) == 1
-                file_path = files_paths[0]
-
-        return file_path
+# Define a function to
