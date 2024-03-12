c93aa6b9ba48afdb9d71b45e2c411eeb506d9099
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:44 +0530

    comment

--- cpp/include/tensorrt_llm/runtime/gptDecoder.h
+++ cpp/include/tensorrt_llm/runtime/gptDecoder.h
@@ -43,42 +43,57 @@ namespace runtime
 class IGptDecoder
 {
 public:
+    // Destructor
     virtual ~IGptDecoder() = default;
 
+    // Set up the decoder with the given sampling configuration and batch size
     virtual void setup(SamplingConfig const& samplingConfig, size_t batchSize) = 0;
 
+    // Perform forward pass through the decoder with the given input and output
     virtual bool forward(DecodingOutput& output, DecodingInput const& input) = 0;
 
+    // Perform forward pass asynchronously through the decoder with the given input and output
     virtual void forwardAsync(DecodingOutput& output, DecodingInput const& input) = 0;
 
+    // Gather tree of final output IDs
     static void gatherTree(ITensor& finalOutputIds, DecodingOutput const& decodingOutput,
         DecodingInput const& decodingInput, BufferManager const& manager);
 
+    // Create a new instance of the decoder with the given data type, vocabulary size, padded vocabulary size, and CUDA stream
     static std::unique_ptr<IGptDecoder> create(
         nvinfer1::DataType dtype, size_t vocabSize, size_t vocabSizePadded, BufferManager::CudaStreamPtr const& stream);
 };
 
+// Template specialization of the IGptDecoder interface for a specific data type
 template <typename T>
 class GptDecoder : public virtual IGptDecoder
 {
 
 public:
-    using CudaStreamPtr = BufferManager::CudaStreamPtr;
-
+    // Constructor
     GptDecoder(size_t vocabSize, size_t vocabSizePadded, CudaStreamPtr const& stream);
 
+    // Set up the decoder with the given sampling configuration and batch size
     void setup(SamplingConfig const& samplingConfig, size_t batchSize) override;
 
+    // Perform forward pass through the decoder with the given input and output
     bool forward(DecodingOutput& output, DecodingInput const& input) override;
 
+    // Perform forward pass asynchronously through the decoder with the given input and output
     void forwardAsync(DecodingOutput& output, DecodingInput const& input) override;
 
 private:
+    // Buffer manager for handling memory allocation and deallocation
     BufferManager mManager;
+
+    // CUDA allocator for handling memory allocation and deallocation
     common::CudaAllocator mAllocator;
+
+    // Dynamic decode layer for performing the actual decoding
     std::shared_ptr<tensorrt_llm::layers::DynamicDecodeLayer<T>> mDynamicDecodeLayer;
 };
 
+// Create a new instance of the IGptDecoder interface for a specific data type
 inline std::unique_ptr<IGptDecoder> IGptDecoder::create(
     nvinfer1::DataType dtype, size_t vocabSize, size_t vocabSizePadded, BufferManager::CudaStreamPtr const& stream)
 {
@@ -91,3 +106,4 @@ inline std::unique_ptr<IGptDecoder> IGptDecoder::create(
 }
 } // namespace runtime
 } // namespace tensorrt_llm
+
