5df5d65018dcd089aefe3279177dcb57cea8dc6a
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:20 +0530

    improve

--- cpp/tensorrt_llm/plugins/ncclPlugin/recvPlugin.cpp
+++ cpp/tensorrt_llm/plugins/ncclPlugin/recvPlugin.cpp
@@ -14,24 +14,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "recvPlugin.h"
 
 using namespace nvinfer1;
 using tensorrt_llm::plugins::RecvPluginCreator;
 using tensorrt_llm::plugins::RecvPlugin;
 
-static const char* RECV_PLUGIN_VERSION{"1"};
-static const char* RECV_PLUGIN_NAME{"Recv"};
-PluginFieldCollection RecvPluginCreator::mFC{};
-std::vector<nvinfer1::PluginField> RecvPluginCreator::mPluginAttributes;
+// Constants
+const char* RECV_PLUGIN_VERSION = "1";
+const char* RECV_PLUGIN_NAME = "Recv";
 
-RecvPlugin::RecvPlugin(int srcRank, nvinfer1::DataType type)
+// PluginFieldCollection and PluginField declarations
+static PluginFieldCollection gPluginFieldsCollection{};
+static std::vector<PluginField> gPluginAttributes{
+    PluginField("src_rank", nullptr, PluginFieldType::kINT32, 1),
+    PluginField("type_id", nullptr, PluginFieldType::kINT32, 1)
+};
+
+// RecvPlugin class
+RecvPlugin::RecvPlugin(int srcRank, DataType type)
     : mSrcRank(srcRank)
     , mType(type)
 {
 }
 
-// Parameterized constructor
 RecvPlugin::RecvPlugin(const void* data, size_t length)
 {
     const char *d = reinterpret_cast<const char*>(data), *a = d;
@@ -40,7 +47,6 @@ RecvPlugin::RecvPlugin(const void* data, size_t length)
     TLLM_CHECK(d == a + length);
 }
 
-// IPluginV2DynamicExt Methods
 nvinfer1::IPluginV2DynamicExt* RecvPlugin::clone() const noexcept
 {
     auto* plugin = new RecvPlugin(*this);
@@ -110,129 +116,4 @@ const char* RecvPlugin::getPluginVersion() const noexcept
 
 int RecvPlugin::getNbOutputs() const noexcept
 {
-    return 1;
-}
-
-int RecvPlugin::initialize() noexcept
-{
-    if (isBuilding())
-    {
-        return 0;
-    }
-    int myRank, nRanks;
-    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &myRank));
-    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &nRanks));
-
-    ncclUniqueId id;
-    MPI_Status status;
-    MPICHECK(MPI_Recv(&id, sizeof(id), MPI_BYTE, mSrcRank, 0, MPI_COMM_WORLD, &status));
-    NCCLCHECK(ncclCommInitRank(&mComm, 2, id, 1));
-    return 0;
-}
-
-void RecvPlugin::terminate() noexcept
-{
-    if (isBuilding())
-    {
-        return;
-    }
-    NCCLCHECK(ncclCommDestroy(mComm));
-}
-
-size_t RecvPlugin::getSerializationSize() const noexcept
-{
-    return sizeof(mSrcRank) + sizeof(mType);
-}
-
-void RecvPlugin::serialize(void* buffer) const noexcept
-{
-    char *d = static_cast<char*>(buffer), *a = d;
-    write(d, mType);
-    write(d, mSrcRank);
-    assert(d == a + getSerializationSize());
-}
-
-void RecvPlugin::destroy() noexcept
-{
-    // This gets called when the network containing plugin is destroyed
-    delete this;
-}
-
-///////////////
-
-RecvPluginCreator::RecvPluginCreator()
-{
-    // Fill PluginFieldCollection with PluginField arguments metadata
-    mPluginAttributes.clear();
-    mPluginAttributes.emplace_back(PluginField("src_rank", nullptr, PluginFieldType::kINT32, 1));
-    mPluginAttributes.emplace_back(PluginField("type_id", nullptr, PluginFieldType::kINT32, 1));
-
-    mFC.nbFields = mPluginAttributes.size();
-    mFC.fields = mPluginAttributes.data();
-}
-
-const char* RecvPluginCreator::getPluginName() const noexcept
-{
-    return RECV_PLUGIN_NAME;
-}
-
-const char* RecvPluginCreator::getPluginVersion() const noexcept
-{
-    return RECV_PLUGIN_VERSION;
-}
-
-const PluginFieldCollection* RecvPluginCreator::getFieldNames() noexcept
-{
-    return &mFC;
-}
-
-IPluginV2* RecvPluginCreator::createPlugin(const char* name, const PluginFieldCollection* fc) noexcept
-{
-    const PluginField* fields = fc->fields;
-    int srcRank;
-    nvinfer1::DataType type;
-    // Read configurations from each fields
-    for (int i = 0; i < fc->nbFields; ++i)
-    {
-        const char* attrName = fields[i].name;
-        if (!strcmp(attrName, "src_rank"))
-        {
-            TLLM_CHECK(fields[i].type == PluginFieldType::kINT32);
-            srcRank = static_cast<int>(*(static_cast<const int*>(fields[i].data)));
-        }
-        else if (!strcmp(attrName, "type_id"))
-        {
-            TLLM_CHECK(fields[i].type == PluginFieldType::kINT32);
-            type = static_cast<nvinfer1::DataType>(*(static_cast<const nvinfer1::DataType*>(fields[i].data)));
-        }
-    }
-
-    try
-    {
-        auto* obj = new RecvPlugin(srcRank, type);
-        obj->setPluginNamespace(mNamespace.c_str());
-        return obj;
-    }
-    catch (const std::exception& e)
-    {
-        caughtError(e);
-    }
-    return nullptr;
-}
-
-IPluginV2* RecvPluginCreator::deserializePlugin(const char* name, const void* serialData, size_t serialLength) noexcept
-{
-    // This object will be deleted when the network is destroyed, which will
-    // call RecvPlugin::destroy()
-    try
-    {
-        auto* obj = new RecvPlugin(serialData, serialLength);
-        obj->setPluginNamespace(mNamespace.c_str());
-        return obj;
-    }
-    catch (const std::exception& e)
-    {
-        caughtError(e);
-    }
-    return nullptr;
-}
+    return 1
