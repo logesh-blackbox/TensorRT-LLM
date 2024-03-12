21600061d8d533b3c0edd59df876ffa55f893004
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:35:51 +0530

    improve

--- cpp/include/tensorrt_llm/batch_manager/callbacks.h
+++ cpp/include/tensorrt_llm/batch_manager/callbacks.h
@@ -21,6 +21,7 @@
 #include <memory>
 #include <unordered_set>
 #include <vector>
+#include <string>
 
 namespace tensorrt_llm::batch_manager
 {
@@ -31,7 +32,7 @@ class NamedTensor;
 using GetInferenceRequestsCallback = std::function<std::list<std::shared_ptr<InferenceRequest>>(int32_t)>;
 using SendResponseCallback = std::function<void(uint64_t, std::list<NamedTensor> const&, bool, const std::string&)>;
 using PollStopSignalCallback = std::function<std::unordered_set<uint64_t>()>;
-// json of stats as a string
 using ReturnBatchManagerStatsCallback = std::function<void(const std::string&)>;
 
 } // namespace tensorrt_llm::batch_manager
+
