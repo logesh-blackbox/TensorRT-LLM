49895f2ae2ac876e6b1567fabf437b4624cbb7a7
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:39 +0530

    comment

--- cpp/tensorrt_llm/common/tllmException.h
+++ cpp/tensorrt_llm/common/tllmException.h
@@ -34,12 +34,31 @@ class TllmException : public std::runtime_error
 public:
     static auto constexpr MAX_FRAMES = 128;
 
+    /**
+     * @brief Construct a new Tllm Exception object
+     *
+     * @param file The source file where the exception was thrown
+     * @param line The line number in the source file where the exception was thrown
+     * @param msg The error message
+     */
     explicit TllmException(char const* file, std::size_t line, std::string const& msg);
 
     ~TllmException() noexcept override;
 
+    /**
+     * @brief Get the trace of the exception
+     *
+     * @return std::string The trace of the exception
+     */
     [[nodiscard]] std::string getTrace() const;
 
+    /**
+     * @brief Demangle a symbol name
+     *
+     * @param name The name to demangle
+     *
+     * @return std::string The demangled name
+     */
     static std::string demangle(char const* name);
 
 private:
@@ -48,3 +67,4 @@ private:
 };
 
 } // namespace tensorrt_llm::common
+
