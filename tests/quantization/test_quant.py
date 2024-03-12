bc6873b604778c37d9e94efc3125b438fa5e4577
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:30 +0530

    comment

--- tests/quantization/test_quant.py
+++ tests/quantization/test_quant.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import unittest
 
 from tensorrt_llm.layers import ColumnLinear, RowLinear
@@ -123,3 +124,4 @@ class TestQuant(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
