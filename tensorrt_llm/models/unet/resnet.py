a1a3b36e39de40bc20d446946d71184074afae02
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:23 +0530

    comment

--- tensorrt_llm/models/unet/resnet.py
+++ tensorrt_llm/models/unet/resnet.py
@@ -21,6 +21,18 @@ from ...module import Module
 
 
 class Upsample2D(Module):
+    """
+    Upsample 2D feature maps using either nearest-neighbor interpolation or a
+    transposed convolution.
+
+    Args:
+        channels (int): The number of channels in the input feature maps.
+        use_conv (bool): Whether to use a transposed convolution for upsampling.
+            If True, the `out_channels` argument must be provided.
+        use_conv_transpose (bool): Deprecated. Use `use_conv` instead.
+        out_channels (int, optional): The number of channels in the output
+            feature maps. Required if `use_conv` is True.
+    """
 
     def __init__(self,
                  channels: int,
@@ -44,6 +56,20 @@ class Upsample2D(Module):
             self.conv = None
 
     def forward(self, hidden_states, output_size=None):
+        """
+        Applies the upsampling operation to the input feature maps.
+
+        Args:
+            hidden_states (torch.Tensor): The input feature maps with shape
+                (batch_size, channels, height, width).
+            output_size (Tuple[int, int], optional): The desired output size of
+                the upsampled feature maps. If not provided, nearest-neighbor
+                interpolation with a scale factor of 2 is used.
+
+        Returns:
+            torch.Tensor: The upsampled feature maps with shape
+            (batch_size, out_channels, new_height, new_width).
+        """
         assert not hidden_states.is_dynamic()
         batch, channels, _, _ = hidden_states.size()
         assert channels == self.channels
@@ -67,6 +93,18 @@ class Upsample2D(Module):
 
 
 class Downsample2D(Module):
+    """
+    Downsample 2D feature maps using either average pooling or a convolution.
+
+    Args:
+        channels (int): The number of channels in the input feature maps.
+        use_conv (bool): Whether to use a convolution for downsampling. If True,
+            the `out_channels` argument must be provided.
+        out_channels (int, optional): The number of channels in the output
+            feature maps. Required if `use_conv` is True.
+        padding (int, optional): The amount of padding to apply to the input
+            feature maps before downsampling. Default: 1.
+    """
 
     def __init__(self,
                  channels,
@@ -90,6 +128,17 @@ class Downsample2D(Module):
             self.conv = AvgPool2d(kernel_size=stride, stride=stride)
 
     def forward(self, hidden_states):
+        """
+        Applies the downsampling operation to the input feature maps.
+
+        Args:
+            hidden_states (torch.Tensor): The input feature maps with shape
+                (batch_size, channels, height, width).
+
+        Returns:
+            torch.Tensor: The downsampled feature maps with shape
+            (batch_size, out_channels, new_height, new_width).
+        """
         assert not hidden_states.is_dynamic()
         batch, channels, _, _ = hidden_states.size()
         assert channels == self.channels
@@ -101,129 +150,5 @@ class Downsample2D(Module):
 
 
 class ResnetBlock2D(Module):
-
-    def __init__(
-        self,
-        *,
-        in_channels,
-        out_channels=None,
-        conv_shortcut=False,
-        dropout=0.0,
-        temb_channels=512,
-        groups=32,
-        groups_out=None,
-        pre_norm=True,
-        eps=1e-6,
-        non_linearity="swish",
-        time_embedding_norm="default",
-        kernel=None,
-        output_scale_factor=1.0,
-        use_in_shortcut=None,
-        up=False,
-        down=False,
-    ):
-        super().__init__()
-        self.pre_norm = pre_norm
-        self.pre_norm = True
-        self.in_channels = in_channels
-        out_channels = in_channels if out_channels is None else out_channels
-        self.out_channels = out_channels
-        self.use_conv_shortcut = conv_shortcut
-        self.time_embedding_norm = time_embedding_norm
-        self.up = up
-        self.down = down
-        self.output_scale_factor = output_scale_factor
-
-        if groups_out is None:
-            groups_out = groups
-
-        self.norm1 = GroupNorm(num_groups=groups,
-                               num_channels=in_channels,
-                               eps=eps,
-                               affine=True)
-        self.conv1 = Conv2d(in_channels,
-                            out_channels,
-                            kernel_size=(3, 3),
-                            stride=(1, 1),
-                            padding=(1, 1))
-
-        if temb_channels is not None:
-            self.time_emb_proj = Linear(temb_channels, out_channels)
-        else:
-            self.time_emb_proj = None
-
-        self.norm2 = GroupNorm(num_groups=groups_out,
-                               num_channels=out_channels,
-                               eps=eps,
-                               affine=True)
-        self.conv2 = Conv2d(out_channels,
-                            out_channels,
-                            kernel_size=(3, 3),
-                            stride=(1, 1),
-                            padding=(1, 1))
-
-        if non_linearity == "swish":
-            self.nonlinearity = lambda x: silu(x)
-        elif non_linearity == "mish":
-            self.nonlinearity = Mish()
-        elif non_linearity == "silu":
-            self.nonlinearity = silu
-
-        self.upsample = self.downsample = None
-        #TODO (guomingz) add the fir kernel supporting.
-        if self.up:
-            if kernel == "sde_vp":
-                self.upsample = partial(interpolate,
-                                        scale_factor=2.0,
-                                        mode="nearest")
-            else:
-                self.upsample = Upsample2D(in_channels, use_conv=False)
-        elif self.down:
-
-            if kernel == "sde_vp":
-                self.downsample = partial(avg_pool2d, kernel_size=2, stride=2)
-            else:
-                self.downsample = Downsample2D(in_channels,
-                                               use_conv=False,
-                                               padding=1,
-                                               name="op")
-
-        self.use_in_shortcut = self.in_channels != self.out_channels if use_in_shortcut is None else use_in_shortcut
-
-        if self.use_in_shortcut:
-            self.conv_shortcut = Conv2d(in_channels,
-                                        out_channels,
-                                        kernel_size=(1, 1),
-                                        stride=(1, 1),
-                                        padding=(0, 0))
-        else:
-            self.conv_shortcut = None
-
-    def forward(self, input_tensor, temb):
-        hidden_states = input_tensor
-        hidden_states = self.norm1(hidden_states)
-        hidden_states = self.nonlinearity(hidden_states)
-
-        if self.upsample is not None:
-            input_tensor = self.upsample(input_tensor)
-            hidden_states = self.upsample(hidden_states)
-        elif self.downsample is not None:
-            input_tensor = self.downsample(input_tensor)
-            hidden_states = self.downsample(hidden_states)
-        hidden_states = self.conv1(hidden_states)
-        if temb is not None:
-            temb = self.time_emb_proj(self.nonlinearity(temb))
-            new_shape = list(temb.size())
-            new_shape.extend([1, 1])  #[:,:,None,None] ->view
-            hidden_states = hidden_states + view(temb, new_shape)
-
-        hidden_states = self.norm2(hidden_states)
-        hidden_states = self.nonlinearity(hidden_states)
-        hidden_states = self.conv2(hidden_states)
-
-        if self.conv_shortcut is not None:
-            input_tensor = self.conv_shortcut(input_tensor)
-
-        output_tensor = (input_tensor +
-                         hidden_states) / self.output_scale_factor
-        return output_tensor
+    """
+    A residual block for 2D feature maps with pre
