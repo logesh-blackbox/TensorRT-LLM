8772bfb983bf538db87a866f94e9fc2cc4c22385
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:42 +0530

    comment

--- cpp/include/tensorrt_llm/runtime/cudaEvent.h
+++ cpp/include/tensorrt_llm/runtime/cudaEvent.h
@@ -30,73 +30,53 @@ namespace tensorrt_llm::runtime
 class CudaEvent
 {
 public:
-    using pointer = cudaEvent_t;
-
-    //! Creates a new cuda event. The event will be destroyed in the destructor.
-    //!
-    //! \param flags Flags for event creation. By default, event timing is disabled.
+    // Creates a new cuda event. The event will be destroyed in the destructor.
+    //
+    // \param flags Flags for event creation. By default, event timing is disabled.
     explicit CudaEvent(unsigned int flags = cudaEventDisableTiming)
     {
-        pointer event;
-        TLLM_CUDA_CHECK(::cudaEventCreate(&event, flags));
-        TLLM_LOG_TRACE("Created event %p", event);
-        bool constexpr ownsEvent{true};
-        mEvent = EventPtr{event, Deleter{ownsEvent}};
+        // Create a new CUDA event
+        TLLM_CUDA_CHECK(::cudaEventCreate(&mEvent, flags));
+        TLLM_LOG_TRACE("Created event %p", mEvent);
     }
 
-    //! Pass an existing cuda event to this object.
-    //!
-    //! \param event The event to pass to this object.
-    //! \param ownsEvent Whether this object owns the event and destroys it in the destructor.
-    explicit CudaEvent(pointer event, bool ownsEvent = true)
+    // Pass an existing cuda event to this object.
+    //
+    // \param event The event to pass to this object.
+    // \param ownsEvent Whether this object owns the event and destroys it in the destructor.
+    explicit CudaEvent(cudaEvent_t event, bool ownsEvent = true)
     {
         TLLM_CHECK_WITH_INFO(event != nullptr, "event is nullptr");
-        mEvent = EventPtr{event, Deleter{ownsEvent}};
+        mEvent = event;
+        mOwnsEvent = ownsEvent;
     }
 
-    //! Returns the event associated with this object.
-    [[nodiscard]] pointer get() const
+    // Returns the event associated with this object.
+    [[nodiscard]] cudaEvent_t get() const
     {
-        return mEvent.get();
+        return mEvent;
     }
 
-    //! \brief Synchronizes the event.
+    // Synchronizes the event.
     void synchronize() const
     {
         TLLM_CUDA_CHECK(::cudaEventSynchronize(get()));
     }
 
-private:
-    class Deleter
+    // Destructor, destroys the event if this object owns it.
+    ~CudaEvent()
     {
-    public:
-        explicit Deleter(bool ownsEvent)
-            : mOwnsEvent{ownsEvent}
+        if (mOwnsEvent)
         {
+            TLLM_CUDA_CHECK(::cudaEventDestroy(mEvent));
+            TLLM_LOG_TRACE("Destroyed event %p", mEvent);
         }
+    }
 
-        explicit Deleter()
-            : Deleter{true}
-        {
-        }
-
-        constexpr void operator()(pointer event) const
-        {
-            if (mOwnsEvent && event != nullptr)
-            {
-                TLLM_CUDA_CHECK(::cudaEventDestroy(event));
-                TLLM_LOG_TRACE("Destroyed event %p", event);
-            }
-        }
-
-    private:
-        bool mOwnsEvent;
-    };
-
-    using element_type = std::remove_pointer_t<pointer>;
-    using EventPtr = std::unique_ptr<element_type, Deleter>;
-
-    EventPtr mEvent;
+private:
+    cudaEvent_t mEvent;
+    bool mOwnsEvent;
 };
 
 } // namespace tensorrt_llm::runtime
+
