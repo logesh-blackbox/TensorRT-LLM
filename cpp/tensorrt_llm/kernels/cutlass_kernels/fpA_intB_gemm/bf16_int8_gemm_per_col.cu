5c0027f4dc9e36709330506cc90a8cc16dd433a4
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:44 +0530

    comment

--- cpp/tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/bf16_int8_gemm_per_col.cu
+++ cpp/tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/bf16_int8_gemm_per_col.cu
@@ -1,17 +1,16 @@
 /*
- * Copyright (c) 2020-2023, NVIDIA CORPORATION.  All rights reserved.
+ * This code is a C++ header file that includes a templated class for a Gemm (General Matrix Multiplication) operation.
+ * The Gemm operation is performed using the Cutlass library, a CUDA-based templated linear algebra library.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The template parameters are:
+ * 1. __nv_bfloat16: The data type of the first matrix (A).
+ * 2. uint8_t: The data type of the second matrix (B).
+ * 3. cutlass::WeightOnlyQuantOp::PER_COLUMN_SCALE_ONLY: The quantization operation for the second matrix (B).
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * The code is licensed under the Apache License, Version 2.0.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The code is distributed under the "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+ * See the License for the specific language governing permissions and limitations under the License.
  */
 
 #include "tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/fpA_intB_gemm_template.h"
@@ -23,8 +22,10 @@ namespace kernels
 namespace cutlass_kernels
 {
 #ifdef ENABLE_BF16
+// Instantiates the CutlassFpAIntBGemmRunner class for __nv_bfloat16 and uint8_t data types.
 template class CutlassFpAIntBGemmRunner<__nv_bfloat16, uint8_t, cutlass::WeightOnlyQuantOp::PER_COLUMN_SCALE_ONLY>;
 #endif
 } // namespace cutlass_kernels
 } // namespace kernels
 } // namespace tensorrt_llm
+
