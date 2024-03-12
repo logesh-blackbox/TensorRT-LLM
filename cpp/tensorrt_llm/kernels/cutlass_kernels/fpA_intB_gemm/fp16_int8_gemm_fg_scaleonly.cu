dce3472b085e0de48db075a223453afd8831e280
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:45 +0530

    comment

--- cpp/tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/fp16_int8_gemm_fg_scaleonly.cu
+++ cpp/tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/fp16_int8_gemm_fg_scaleonly.cu
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2020-2023, NVIDIA CORPORATION.  All rights reserved.
+ * This code is a C++ header file that includes a templated class named CutlassFpAIntBGemmRunner. 
+ * The class is used for performing a general matrix multiplication (GEMM) operation with floating-point 
+ * type A (half) and integer type B (uint8_t). The GEMM operation is implemented using the Cutlass library, 
+ * which is a CUDA-focused linear algebra library developed by NVIDIA.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The class is defined within the namespace tensorrt_llm::kernels::cutlass_kernels.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * The template parameters are:
+ * 1. half: The floating-point type A for the GEMM operation.
+ * 2. uint8_t: The integer type B for the GEMM operation.
+ * 3. cutlass::WeightOnlyQuantOp::FINEGRAINED_SCALE_ONLY: The quantization operation for the weights.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The header file is protected by the Apache License, Version 2.0, which provides users with permissions 
+ * and limitations regarding the use, reproduction, and distribution of the code.
  */
 
 #include "tensorrt_llm/kernels/cutlass_kernels/fpA_intB_gemm/fpA_intB_gemm_template.h"
@@ -22,7 +23,9 @@ namespace kernels
 {
 namespace cutlass_kernels
 {
+// The templated class definition is provided here.
 template class CutlassFpAIntBGemmRunner<half, uint8_t, cutlass::WeightOnlyQuantOp::FINEGRAINED_SCALE_ONLY>;
 } // namespace cutlass_kernels
 } // namespace kernels
 } // namespace tensorrt_llm
+
