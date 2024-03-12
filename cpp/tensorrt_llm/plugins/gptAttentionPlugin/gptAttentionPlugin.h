b44a96fcdde89e1925015d5716e6c1fb6ace0c10
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:06 +0530

    comment

--- cpp/tensorrt_llm/plugins/gptAttentionPlugin/gptAttentionPlugin.h
+++ cpp/tensorrt_llm/plugins/gptAttentionPlugin/gptAttentionPlugin.h
@@ -61,180 +61,30 @@ namespace tensorrt_llm::plugins
 //     present_key_value_pool (optional if not paged kv cache) [batch_size, 2, local_num_kv_heads, max_seq_len,
 //     head_size]
 
+// This class implements the GPTAttentionPlugin, which is a plugin for the TensorRT framework.
+// The plugin is used to perform multi-head attention operations in the GPT language model.
+// The plugin takes in a batch of sequences, and for each sequence, it computes the attention
+// weights between each token in the sequence and all other tokens in the sequence.
+// The attention weights are then used to compute a weighted sum of the values of the tokens,
+// which is used as the output for the sequence.
+// The plugin also supports the use of a key-value cache, which can be used to speed up the
+// computation of the attention weights for sequences that have been seen before.
+// The plugin is implemented as a template class, with the template parameter specifying the
+// data type of the input and output tensors.
 class GPTAttentionPlugin : public GPTAttentionPluginCommon
 {
 public:
+    // The constructor takes in various parameters that are used to configure the behavior of
+    // the plugin.
+    // The parameters include the number of attention heads, the size of each attention head,
+    // whether the attention is unidirectional, the scaling factor for the queries, the type
+    // of position embedding to use, the dimension and base for the rotary embedding, the
+    // type of rotary scaling to use, the scale factor for the rotary embedding, the maximum
+    // number of positions for the rotary embedding, the size and rank of the token padding,
+    // the type of context fused multi-head attention to use, whether to use multi-block mode,
+    // the quantization mode for the key-value cache, whether to remove input padding, the
+    // type of attention mask to use, whether to use a paged key-value cache, the number of
+    // tokens per block, the data type of the input and output tensors, the maximum context
+    // length, whether to enable the query-key-value bias, and whether to use cross attention.
     GPTAttentionPlugin(int num_heads, int num_kv_heads, int head_size, int unidirectional, float q_scaling,
-        tensorrt_llm::kernels::PositionEmbeddingType position_embedding_type,
-        int rotary_embedding_dim, // for RoPE. 0 for non-RoPE
-        float rotary_embedding_base, tensorrt_llm::kernels::RotaryScalingType rotary_embedding_scale_type,
-        float rotary_embedding_scale, int rotary_embedding_max_positions, int tp_size, int tp_rank, // for ALiBi
-        tensorrt_llm::kernels::ContextFMHAType context_fmha_type, bool multi_block_mode, int kv_cache_quant_mode,
-        bool remove_input_padding, tensorrt_llm::kernels::AttentionMaskType mask_type, bool paged_kv_cache,
-        int tokens_per_block, nvinfer1::DataType type, int32_t max_context_length, bool qkv_bias_enabled,
-        bool cross_attention = false, int max_distance = 0);
-
-    GPTAttentionPlugin(const void* data, size_t length);
-
-    ~GPTAttentionPlugin() override = default;
-
-    // IPluginV2DynamicExt Methods
-    nvinfer1::DimsExprs getOutputDimensions(int outputIndex, const nvinfer1::DimsExprs* inputs, int nbInputs,
-        nvinfer1::IExprBuilder& exprBuilder) noexcept override;
-
-    bool supportsFormatCombination(
-        int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) noexcept override;
-    void configurePlugin(const nvinfer1::DynamicPluginTensorDesc* in, int nbInputs,
-        const nvinfer1::DynamicPluginTensorDesc* out, int nbOutputs) noexcept override;
-    size_t getWorkspaceSize(const nvinfer1::PluginTensorDesc* inputs, int nbInputs,
-        const nvinfer1::PluginTensorDesc* outputs, int nbOutputs) const noexcept override;
-    int enqueue(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
-        const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept override;
-
-    template <typename T, typename KVCacheBuffer>
-    int enqueueImpl(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
-        const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream);
-
-    template <typename T>
-    int enqueueDispatchKVCacheType(const nvinfer1::PluginTensorDesc* inputDesc,
-        const nvinfer1::PluginTensorDesc* outputDesc, const void* const* inputs, void* const* outputs, void* workspace,
-        cudaStream_t stream);
-
-    // IPluginV2Ext Methods
-    nvinfer1::DataType getOutputDataType(
-        int index, const nvinfer1::DataType* inputTypes, int nbInputs) const noexcept override;
-
-    // IPluginV2 Methods
-    const char* getPluginType() const noexcept override;
-    const char* getPluginVersion() const noexcept override;
-    int getNbOutputs() const noexcept override;
-
-    //! This is called on every trt ExecutionContext creation by TRT
-    //! Note TRT does not call the initialize on cloned plugin, so clone internally should do initialization.
-    GPTAttentionPlugin* clone() const noexcept override;
-
-    size_t getSerializationSize() const noexcept override;
-    void serialize(void* buffer) const noexcept override;
-
-    enum class RequestType : int32_t
-    {
-        kCONTEXT = 0,
-        kGENERATION = 1
-    };
-
-private:
-    template <typename T, typename KVCacheBuffer>
-    int enqueueSome(int32_t seqIdxBeg, int32_t localNbSeq, int32_t tokenIdxBeg, int32_t localNbTokens,
-        const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
-        const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream);
-
-    using IndexType = std::int32_t;
-
-    IndexType getInputTensorIdx() const
-    {
-        return 0;
-    }
-
-    IndexType getSequenceLengthIdx() const
-    {
-        return 1;
-    }
-
-    IndexType getHostPastKeyValueLengthsIdx() const
-    {
-        return 2;
-    }
-
-    IndexType getContextLengthsIdx() const
-    {
-        return 3;
-    }
-
-    IndexType getCacheIndirIdx() const
-    {
-        return 4;
-    }
-
-    IndexType getRequestTypesIdx() const
-    {
-        return 5;
-    }
-
-    IndexType getKVCacheBlockPointersIdx() const
-    {
-        // NOTE We either provide this tensor when mPagedKVCache is true or PastKeyValue otherwise
-        return 6;
-    }
-
-    IndexType getPastKeyValueIdx() const
-    {
-        // NOTE We either provide this tensor when mPagedKVCache is false or KVCacheBlockPointers otherwise
-        return 6;
-    }
-
-    IndexType getKVCacheQuantizationScaleIdx() const
-    {
-        return 7;
-    }
-
-    IndexType getKVCacheDequantizationScaleIdx() const
-    {
-        return 8;
-    }
-
-    IndexType getAlibiSlopesIdx() const
-    {
-        return (mKVCacheQuantMode.hasKvCacheQuant() ? 9 : 7);
-    }
-
-    IndexType getRelativeAttentionBiasIdx() const
-    {
-        return getAlibiSlopesIdx() + (isALiBi() ? 1 : 0);
-    }
-
-    IndexType getCrossQKVIdx() const
-    {
-        return getRelativeAttentionBiasIdx() + (isRelativePosition() ? 1 : 0);
-    }
-
-    IndexType getCrossQKVLengthIdx() const
-    {
-        return getCrossQKVIdx() + 1;
-    }
-
-    IndexType getEncoderInputLengthsIdx() const
-    {
-        return getCrossQKVLengthIdx() + 1;
-    }
-
-    IndexType getHostContextLengthsIdx() const
-    {
-        TLLM_CHECK(mRemovePadding);
-        return getCrossQKVIdx() + (isCrossAttention() ? 3 : 0);
-    }
-
-    IndexType getQKVBiasTensorIdx() const
-    {
-        TLLM_CHECK(mQKVBiasEnabled);
-        return (mKVCacheQuantMode.hasKvCacheQuant() ? 9 : 7) + (isALiBi() ? 1 : 0) + (mRemovePadding ? 1 : 0);
-    }
-};
-
-class GPTAttentionPluginCreator : public GPTAttentionPluginCreatorCommon
-{
-public:
-    GPTAttentionPluginCreator();
-
-    const char* getPluginName() const noexcept override;
-
-    const char* getPluginVersion() const noexcept override;
-
-    const nvinfer1::PluginFieldCollection* getFieldNames() noexcept override;
-
-    nvinfer1::IPluginV2* createPlugin(const char* name, const nvinfer1::PluginFieldCollection* fc) noexcept override;
-
-    nvinfer1::IPluginV2* deserializePlugin(
-        const char* name, const void* serialData, size_t serialLength) noexcept override;
-};
-
-} // namespace tensorrt_llm::plugins
+        tensorrt_llm::kernels::PositionEmbeddingType position_
