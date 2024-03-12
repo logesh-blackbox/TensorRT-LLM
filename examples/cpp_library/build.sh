bad6e56bb5f077f4544d9e20118cdc7d3f82a611
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:19 +0530

    improve

--- examples/cpp_library/build.sh
+++ examples/cpp_library/build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 BUILD_DIR="build"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-rm -rf ${BUILD_DIR} && mkdir -p ${BUILD_DIR}
+rm -rf "${BUILD_DIR}" && mkdir -p "${BUILD_DIR}"
 
-pushd ${BUILD_DIR}
+pushd "${BUILD_DIR}"
 
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     ..
 
-make -j"$(grep -c ^processor /proc/cpuinfo)"
+make -j"$(nproc)"
 
 export LD_LIBRARY_PATH="${SCRIPT_DIR}:${LD_LIBRARY_PATH}"
 
