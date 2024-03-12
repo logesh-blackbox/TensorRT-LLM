d6edda58a08838ceacaf84c43e44f68602fdbbcb
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:35 +0530

    improve

--- tensorrt_llm/layers/embedding.py
+++ tensorrt_llm/layers/embedding.py
@@ -41,7 +41,6 @@ class Embedding(Module):
                  sharding_dim=0,
                  tp_rank=None):
         super().__init__()
-        # num_embeddings records the total vocab size no matter using TP or not
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
         self.tp_size = tp_size
@@ -67,7 +66,7 @@ class Embedding(Module):
                          tp_rank=self.tp_rank)
 
 
-class PromptTuningEmbedding(Embedding):
+class PromptTuningEmbedding(Module):
     """
         Pass all tokens though both normal and prompt embedding tables.
     Then, combine results based on whether the token was "normal" or "prompt/virtual".
@@ -82,34 +81,14 @@ class PromptTuningEmbedding(Embedding):
                  tp_group=None,
                  sharding_dim=0,
                  tp_rank=0):
-        super().__init__(num_embeddings, embedding_dim, dtype, tp_size,
-                         tp_group, sharding_dim, tp_rank)
+        super().__init__()
         if vocab_size is None:
             vocab_size = num_embeddings
         self.vocab_size = vocab_size
+        self.embedding = Embedding(num_embeddings, embedding_dim, dtype, tp_size,
+                                   tp_group, sharding_dim, tp_rank)
+        self.prompt_embedding = Embedding(vocab_size, embedding_dim, dtype, tp_size,
+                                          tp_group, sharding_dim, tp_rank)
 
-    def forward(self, tokens, prompt_embedding_table, tasks, task_vocab_size):
-        # do not use ">=" because internally the layer works with floating points
-        prompt_tokens_mask = tokens > (self.vocab_size - 1)
-
-        # clip tokens in the [0, vocab_size) range
-        normal_tokens = where(prompt_tokens_mask, self.vocab_size - 1, tokens)
-        normal_embeddings = embedding(normal_tokens, self.weight.value,
-                                      self.tp_size, self.tp_group,
-                                      self.sharding_dim, self.tp_rank)
-
-        # put virtual tokens in the [0, max_prompt_vocab_size) range
-        prompt_tokens = where(prompt_tokens_mask, tokens - self.vocab_size, 0)
-
-        # add offsets to match the concatenated embedding tables
-        tasks = tasks * task_vocab_size
-
-        # tasks: [batch_size, seq_len]
-        # prompt_tokens: [batch_size, seq_len]
-        prompt_tokens = prompt_tokens + tasks
-        prompt_embeddings = embedding(prompt_tokens, prompt_embedding_table)
-
-        # prompt_tokens_mask: [batch_size, seq_len] -> [batch_size, seq_len, 1]
-        # combine the correct sources of embedding: normal/prompt
-        return where(unsqueeze(prompt_tokens_mask, -1), prompt_embeddings,
-                     normal_embeddings)
+    def forward(self, tokens, prompt_embedding_table):
+        prompt_tokens_mask = tokens > (
