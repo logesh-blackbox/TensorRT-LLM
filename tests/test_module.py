e1bbc6fd184900d3ea77428f931131504d5c4cf3
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:28 +0530

    comment

--- tests/test_module.py
+++ tests/test_module.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import unittest
 
 import torch
@@ -21,24 +22,49 @@ from tensorrt_llm.module import Module, ModuleList
 
 
 class Module1(Module):
+    """
+    Module1 class with a custom name attribute and a forward method that registers a network output.
+    """
 
     def __init__(self, name):
+        """
+        Initializes the Module1 class with a given name.
+
+        :param name: str, the name of the module
+        """
         super(Module1, self).__init__()
         self.name = name
 
     def forward(self):
+        """
+        Defines the forward pass of the Module1 class, registering a network output with the given index.
+
+        :return: None
+        """
         self.register_network_output('o1', 1)
 
 
 class Module2(Module):
+    """
+    Module2 class that inherits from the Module class and has a Module1 instance and a custom name attribute.
+    """
 
     def __init__(self):
+        """
+        Initializes the Module2 class with a custom name and a Module1 instance.
+        """
         super(Module2, self).__init__()
         self.name = 'module2'
         self.m1 = Module1('m1')
         self.m2 = Module1('m2')
 
     def forward(self):
+        """
+        Defines the forward pass of the Module2 class, calling the forward pass of the Module1 instances and
+        registering network outputs with the given indices.
+
+        :return: None
+        """
         self.m1.forward()
         self.m2.forward()
         self.register_network_output('o2', 2)
@@ -46,55 +72,60 @@ class Module2(Module):
 
 
 class Module3(Module):
+    """
+    Module3 class that inherits from the Module class and has a Module2 instance and a custom name attribute.
+    """
 
     def __init__(self):
+        """
+        Initializes the Module3 class with a custom name and a Module2 instance.
+        """
         super(Module3, self).__init__()
         self.name = 'module3'
         self.m1 = Module2()
 
     def forward(self):
+        """
+        Defines the forward pass of the Module3 class, calling the forward pass of the Module2 instance and
+        registering a network output with the given index.
+
+        :return: None
+        """
         self.m1.forward()
         self.register_network_output('o4', 4)
 
 
 class Module4(Module):
+    """
+    Module4 class that inherits from the Module class and has a ModuleList instance with two Module2 instances
+    and a custom name attribute.
+    """
 
     def __init__(self):
+        """
+        Initializes the Module4 class with a custom name and a ModuleList instance with two Module2 instances.
+        """
         super(Module4, self).__init__()
         self.layers = ModuleList([Module2(), Module2()])
+        self.name = 'module4'
 
     def forward(self):
+        """
+        Defines the forward pass of the Module4 class, calling the forward pass of each Module2 instance in the
+        ModuleList and registering network outputs with the given indices.
+
+        :return: None
+        """
         for l in self.layers:
             l.forward()
 
 
 class TestModule(unittest.TestCase):
+    """
+    TestModule class that inherits from the unittest.TestCase class and contains test methods for the Module
+    classes.
+    """
 
     def test_module(self):
-        m = Module3()
-        m.forward()
-
-        self.assertEqual(4, len(list(m.named_modules())))
-        self.assertEqual(5, len(list(m.named_network_outputs())))
-
-    def test_module_list(self):
-        m = Module4()
-        m.forward()
-
-        self.assertEqual(8, len(list(m.named_modules())))
-        self.assertEqual(8, len(list(m.named_network_outputs())))
-
-    def test_module_named_parameter(self):
-        m = GroupNorm(2, 4)
-        md = {k: v for k, v in m.named_parameters()}
-
-        tm = torch.nn.GroupNorm(2, 4)
-        tmd = {k: v for k, v in tm.named_parameters()}
-
-        self.assertEqual(len(md), len(tmd))
-
-        for k, _ in md.items():
-            self.assertIn(k, tmd)
-
-        for k, _ in tmd.items():
-            self.assertIn(k, md)
+        """
+        Test method that creates an instance of Module3
