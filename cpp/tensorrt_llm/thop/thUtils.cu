e4bf2b660687f4d6c609c1997cf617df150e2203
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:05 +0530

    comment

--- cpp/tensorrt_llm/thop/thUtils.cu
+++ cpp/tensorrt_llm/thop/thUtils.cu
@@ -19,6 +19,12 @@
 namespace torch_ext
 {
 
+/**
+ * Convert the given Tensor's shape to a vector of size_t.
+ *
+ * @param tensor The Tensor to convert the shape of.
+ * @return A vector of size_t representing the shape of the given Tensor.
+ */
 std::vector<size_t> convert_shape(torch::Tensor tensor)
 {
     std::vector<size_t> v_shape;
@@ -29,26 +35,15 @@ std::vector<size_t> convert_shape(torch::Tensor tensor)
     return v_shape;
 }
 
-template <typename T>
-tensorrt_llm::common::Tensor convert_tensor(torch::Tensor tensor)
-{
-    tensorrt_llm::common::MemoryType mtype
-        = tensor.is_cuda() ? tensorrt_llm::common::MEMORY_GPU : tensorrt_llm::common::MEMORY_CPU;
-    return convert_tensor<T>(tensor, mtype);
-}
-
-// Template instantiations
-template tensorrt_llm::common::Tensor convert_tensor<int8_t>(torch::Tensor tensor);
-template tensorrt_llm::common::Tensor convert_tensor<float>(torch::Tensor tensor);
-template tensorrt_llm::common::Tensor convert_tensor<half>(torch::Tensor tensor);
-#ifdef ENABLE_BF16
-template tensorrt_llm::common::Tensor convert_tensor<__nv_bfloat16>(torch::Tensor tensor);
-#endif
-template tensorrt_llm::common::Tensor convert_tensor<int>(torch::Tensor tensor);
-template tensorrt_llm::common::Tensor convert_tensor<unsigned long long int>(torch::Tensor tensor);
-template tensorrt_llm::common::Tensor convert_tensor<unsigned int>(torch::Tensor tensor);
-template tensorrt_llm::common::Tensor convert_tensor<bool>(torch::Tensor tensor);
-
+/**
+ * Convert the given Tensor to a TensorRT common::Tensor with the specified memory type.
+ *
+ * @tparam T The data type of the Tensor to convert.
+ * @param tensor The Tensor to convert.
+ * @param memory_type The memory type of the resulting TensorRT common::Tensor.
+ * @return A TensorRT common::Tensor with the specified memory type and data type,
+ *         containing the same data as the given Tensor.
+ */
 template <typename T>
 tensorrt_llm::common::Tensor convert_tensor(torch::Tensor tensor, tensorrt_llm::common::MemoryType memory_type)
 {
@@ -76,9 +71,44 @@ template tensorrt_llm::common::Tensor convert_tensor<unsigned int>(
 template tensorrt_llm::common::Tensor convert_tensor<bool>(
     torch::Tensor tensor, tensorrt_llm::common::MemoryType memory_type);
 
+/**
+ * Convert the given Tensor to a TensorRT common::Tensor with the default memory type.
+ *
+ * @tparam T The data type of the Tensor to convert.
+ * @param tensor The Tensor to convert.
+ * @return A TensorRT common::Tensor with the default memory type and data type,
+ *         containing the same data as the given Tensor.
+ */
+template <typename T>
+tensorrt_llm::common::Tensor convert_tensor(torch::Tensor tensor)
+{
+    tensorrt_llm::common::MemoryType mtype
+        = tensor.is_cuda() ? tensorrt_llm::common::MEMORY_GPU : tensorrt_llm::common::MEMORY_CPU;
+    return convert_tensor<T>(tensor, mtype);
+}
+
+// Template instantiations
+template tensorrt_llm::common::Tensor convert_tensor<int8_t>(torch::Tensor tensor);
+template tensorrt_llm::common::Tensor convert_tensor<float>(torch::Tensor tensor);
+template tensorrt_llm::common::Tensor convert_tensor<half>(torch::Tensor tensor);
+#ifdef ENABLE_BF16
+template tensorrt_llm::common::Tensor convert_tensor<__nv_bfloat16>(torch::Tensor tensor);
+#endif
+template tensorrt_llm::common::Tensor convert_tensor<int>(torch::Tensor tensor);
+template tensorrt_llm::common::Tensor convert_tensor<unsigned long long int>(torch::Tensor tensor);
+template tensorrt_llm::common::Tensor convert_tensor<unsigned int>(torch::Tensor tensor);
+template tensorrt_llm::common::Tensor convert_tensor<bool>(torch::Tensor tensor);
+
+/**
+ * Get the size of the given Tensor in bytes.
+ *
+ * @param tensor The Tensor to get the size of.
+ * @return The size of the given Tensor in bytes.
+ */
 size_t sizeBytes(torch::Tensor tensor)
 {
     return tensor.numel() * torch::elementSize(torch::typeMetaToScalarType(tensor.dtype()));
 }
 
 } // namespace torch_ext
+
