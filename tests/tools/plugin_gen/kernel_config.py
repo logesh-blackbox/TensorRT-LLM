ea5600ac7988b39c8b534d3ce8325bf4507d9954
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:29 +0530

    comment

--- tests/tools/plugin_gen/kernel_config.py
+++ tests/tools/plugin_gen/kernel_config.py
@@ -1,21 +1,23 @@
-import os
+# This script is used to generate kernel metadata for TensorRT.
 
+import os
 from tensorrt_llm.tools.plugin_gen.core import *
 
+# The root directory of the OpenAI Triton manual plugin examples.
 openai_triton_example_root = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "examples",
     "openai_triton", "manual_plugin")
 
-
+# The function `get_fmha_kernel_meta_data` returns the metadata of the 'fused_attention_kernel'.
 def get_fmha_kernel_meta_data():
     return KernelMetaData(
-        kernel_name='fused_attention_kernel',
+        kernel_name='fused_attention_kernel',  # The name of the kernel.
         ios=[
-            # outputs
+            # Output arguments.
             OutputArg('Out', Type('tensor[fp16]'), hints=['16', '16']),
             OutputArg('L', Type('tensor[fp32]'), hints=['16', '16']),
             OutputArg('M', Type('tensor[fp16]'), hints=['16', '16']),
-            # inputs
+            # Input arguments.
             InputArg('Q', Type('tensor[fp16]'), hints=['16', '16']),
             InputArg('K', Type('tensor[fp16]'), hints=['16', '16']),
             InputArg('V', Type('tensor[fp16]'), hints=['16', '16']),
@@ -23,27 +25,22 @@ def get_fmha_kernel_meta_data():
             DimSizeArg('batch_size'),
             ParamArg('num_heads', Type('i32')),
             DimSizeArg('seq_len', hints=['', '16']),
-            # constexprs
+            # Constexpr arguments.
             Constexpr(128),
             Constexpr(64),
             Constexpr(128),
         ],
         shape_infer_rules=[
-            # The following rules helps to deduce the shapes of the output tensors
+            # The following rules help to deduce the shapes of the output tensors.
             "Q[*] -> Out[*]",
             "Q[m,n,k,*] -> L[m,n,k]",
             "Q[m,n,k,*] -> M[m,n,k]",
 
-            # The following rules helps to deduce both DimSizeArgs: batch_size and seq_len
+            # The following rules help to deduce both DimSizeArgs: batch_size and seq_len.
             "Q[m,n,k,*] : m -> batch_size",
             "Q[m,n,k,*] : k -> seq_len",
         ],
-        version=0,
-        kernel_file=f'{openai_triton_example_root}/fmha_triton.py',
-        num_warps=1,
-        grid_dims=("(seq_len + 127) / 128", "batch_size * num_heads", "1"))
-
-
-KERNELS = [
-    get_fmha_kernel_meta_data(),
-]
+        version=0,  # The version of the kernel.
+        kernel_file=f'{openai_triton_example_root}/fmha_triton.py',  # The path to the kernel file.
+        num_warps=1,  # The number of warps.
+        grid
