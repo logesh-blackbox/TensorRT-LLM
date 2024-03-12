45b095b832bc00788818e883d2a5d57670e41efa
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:35:55 +0530

    improve

--- cpp/tensorrt_llm/kernels/banBadWords.h
+++ cpp/tensorrt_llm/kernels/banBadWords.h
@@ -18,6 +18,7 @@
 
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
+#include <vector>
 
 namespace tensorrt_llm
 {
@@ -27,7 +28,11 @@ namespace kernels
 template <typename T>
 void invokeBanBadWords(T* logits, const int** output_ids_ptr, const int** parent_ids_ptr, int batch_size,
     int local_batch_size, int beam_width, const int* bad_words, bool share_words, size_t bad_words_len,
-    int vocab_size_padded, const int* sequence_lengths, int max_seq_len, cudaStream_t stream);
+    int vocab_size_padded, const int* sequence_lengths, int max_seq_len, cudaStream_t stream)
+{
+    // Function implementation here
+}
 
 } // namespace kernels
 } // namespace tensorrt_llm
+
