5f624a22ff532e88e826832906d475b27009cb75
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:35 +0530

    improve

--- tests/functional/test_softplus.py
+++ tests/functional/test_softplus.py
@@ -32,17 +32,16 @@ class TestFunctional(unittest.TestCase):
         dtype = 'float32'
         x_data = torch.randn(3, 5)
         builder = tensorrt_llm.Builder()
-        net = builder.create_network()
-        with tensorrt_llm.net_guard(net):
-            x = Tensor(name='x',
-                       shape=x_data.shape,
-                       dtype=tensorrt_llm.str_dtype_to_trt(dtype))
-            output = functional.softplus(x, 1.6, 3.2).trt_tensor
-            output.name = 'output'
-            net.mark_output(output)
+        network = builder.create_network()
+        x = Tensor(name='x',
+                   shape=x_data.shape,
+                   dtype=tensorrt_llm.str_dtype_to_trt(dtype))
+        output = functional.softplus(x, 1.6, 3.2).trt_tensor
+        output.name = 'output'
+        network.mark_output(output)
 
-        build_engine = EngineFromNetwork((builder.trt_builder, net.trt_network))
-        with TrtRunner(build_engine) as runner:
+        engine = EngineFromNetwork((builder.trt_builder, network.trt_network))
+        with TrtRunner(engine) as runner:
             outputs = runner.infer(feed_dict={
                 'x': x_data.numpy(),
             })
@@ -52,4 +51,5 @@ class TestFunctional(unittest.TestCase):
         np.testing.assert_allclose(ref.cpu().numpy(),
                                    outputs['output'],
                                    atol=1e-6)
+
 
