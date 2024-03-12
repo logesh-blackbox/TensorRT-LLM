e984928f9e4c82b39592ae1f20190fa24b26c34f
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:33:54 +0530

    comment

--- cpp/tensorrt_llm/layers/onlineBeamSearchLayer.h
+++ cpp/tensorrt_llm/layers/onlineBeamSearchLayer.h
@@ -16,12 +16,10 @@
 
 #pragma once
 
-#include "tensorrt_llm/kernels/onlineSoftmaxBeamsearchKernels.h"
+#include "tensorrt_llm/common.h"
 #include "tensorrt_llm/layers/baseBeamSearchLayer.h"
 
 #include <optional>
-
-namespace tc = tensorrt_llm::common;
 
 namespace tensorrt_llm
 {
@@ -76,3 +74,32 @@ protected:
 
 } // namespace layers
 } // namespace tensorrt_llm
+
+
+
+/*
+ * Copyright (c) 2019-2023, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "tensorrt_llm/common.h"
+#include "tensorrt_llm/layers/baseBeamSearchLayer.h"
+
+#include <optional>
+
+namespace tensorrt_llm
+{
+
