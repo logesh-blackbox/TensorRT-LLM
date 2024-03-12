ef5f958ce86020bbc07233455c326aec4d703bb4
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:11 +0530

    comment

--- tensorrt_llm/quantization/__init__.py
+++ tensorrt_llm/quantization/__init__.py
@@ -1,17 +1,31 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is licensed under the Apache License, Version 2.0.
+# You may not use this file except in compliance with the License.
+# For a copy of the License, visit: http://www.apache.org/licenses/LICENSE-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-from .mode import QuantMode
+# This software is provided "AS IS", without any express or implied
+# warranties or conditions of any kind. Please see the License for
+# the specific language governing permissions and limitations under
+# the License.
 
-__all__ = ['QuantMode']
+# This module, `mode.py`, contains the QuantMode class.
+# The QuantMode class is used to represent different quantization modes.
+#
+# Example:
+#     from .mode import QuantMode
+#     quant_mode = QuantMode.SYMMETRIC
+#
+# The `__all__` variable is used to list all public objects in this module.
+#
+# Variables:
+#     __all__: List[str] = ['QuantMode']
+#
+# Classes:
+#     QuantMode: Enum class representing different quantization modes.
+#
+# References:
+#     - SPDX License Identifier: Apache-2.0
+#     - License: http://www.apache.org/licenses/LICENSE-2.0
+
