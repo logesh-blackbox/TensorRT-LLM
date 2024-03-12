555fd0945b88a3cc44c3bf332d6a2b8aa0993777
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:34 +0530

    comment

--- cpp/tests/common/tensorTest.cpp
+++ cpp/tests/common/tensorTest.cpp
@@ -23,9 +23,7 @@
 
 using namespace tensorrt_llm::common;
 
-namespace
-{
-
+// Define a macro for comparing two tensors
 #define EXPECT_EQUAL_TENSORS(t1, t2)                                                                                   \
     do                                                                                                                 \
     {                                                                                                                  \
@@ -35,6 +33,7 @@ namespace
         EXPECT_TRUE(t1.data == t2.data);                                                                               \
     } while (false)
 
+// Test for checking if a key exists in the TensorMap
 TEST(TensorMapTest, HasKeyCorrectness)
 {
     bool* v1 = new bool(true);
@@ -43,14 +42,19 @@ TEST(TensorMapTest, HasKeyCorrectness)
     Tensor t2 = Tensor{MEMORY_CPU, TYPE_FP32, {3, 2}, v2};
 
     TensorMap map({{"t1", t1}, {"t2", t2}});
+
+    // Check if the keys "t1" and "t2" exist in the map
     EXPECT_TRUE(map.contains("t1"));
     EXPECT_TRUE(map.contains("t2"));
+
+    // Check if the key "t3" does not exist in the map
     EXPECT_FALSE(map.contains("t3"));
 
     delete v1;
     delete[] v2;
 }
 
+// Test for inserting a tensor into the TensorMap
 TEST(TensorMapTest, InsertCorrectness)
 {
     int* v1 = new int[4]{1, 10, 20, 30};
@@ -59,218 +63,64 @@ TEST(TensorMapTest, InsertCorrectness)
     Tensor t2 = Tensor(MEMORY_CPU, TYPE_INT32, {2}, v2);
 
     TensorMap map({{"t1", t1}});
+
+    // Check the size and contents of the map after insertion
     EXPECT_TRUE(map.size() == 1);
     EXPECT_TRUE(map.contains("t1"));
     EXPECT_EQUAL_TENSORS(map.at("t1"), t1);
     EXPECT_FALSE(map.contains("t2"));
+
+    delete[] v1;
+    delete[] v2;
 }
 
+// Test for inserting a tensor with a nonexistent key into the TensorMap
 TEST(TensorMapTest, InsertDoesNotAllowNoneTensor)
 {
     TensorMap map;
-    EXPECT_TRUE(map.size() == 0);
-    // forbid a none tensor.
+
+    // Attempt to insert a nonexistent tensor and check for an exception
     EXPECT_THROW(map.insert("none", {}), std::runtime_error);
 
-    // forbid a tensor having null data pointer.
+    // Attempt to insert a tensor with a null data pointer and check for an exception
     Tensor none_data_tensor = Tensor(MEMORY_CPU, TYPE_INT32, {}, nullptr);
     EXPECT_THROW(map.insert("empty", none_data_tensor), std::runtime_error);
 }
 
+// Test for inserting a tensor with a duplicate key into the TensorMap
 TEST(TensorMapTest, InsertDoesNotAllowDuplicatedKey)
 {
     int* v1 = new int[4]{1, 10, 20, 30};
     Tensor t1 = Tensor(MEMORY_CPU, TYPE_INT32, {4}, v1);
     Tensor t2 = Tensor(MEMORY_CPU, TYPE_INT32, {2}, v1);
     TensorMap map({{"t1", t1}});
-    EXPECT_TRUE(map.size() == 1);
-    // forbid a duplicated key.
+
+    // Attempt to insert a tensor with a duplicate key and check for an exception
     EXPECT_THROW(map.insert("t1", t2), std::runtime_error);
+
     delete[] v1;
 }
 
+// Test for getting a value from the TensorMap
 TEST(TensorMapTest, GetValCorrectness)
 {
     int* v1 = new int[4]{1, 10, 20, 30};
     Tensor t1 = Tensor(MEMORY_CPU, TYPE_INT32, {4}, v1);
 
     TensorMap map({{"t1", t1}});
-    EXPECT_TRUE(map.size() == 1);
-    // throw exception since the map doesn't have a key "t3".
+
+    // Check for an exception when trying to get a nonexistent key
     EXPECT_THROW(map.getVal<int>("t3"), std::runtime_error);
+
+    // Check the value of the tensor with key "t1"
     EXPECT_TRUE(map.getVal<int>("t1") == 1);
+
+    // Check the value of the tensor with key "t1" using a default value
     EXPECT_TRUE(map.getVal<int>("t1", 3) == 1);
 
-    // map doesn't have t2 so return the default value 3.
+    // Check the default value when the key does not exist
     EXPECT_TRUE(map.getVal<int>("t2", 3) == 3);
 
-    v1[0] += 1; // update value.
-    EXPECT_TRUE(map.getVal<int>("t1") == 2);
-    EXPECT_TRUE(map.getVal<int>("t1", 3) == 2);
-
-    size_t index = 2;
-    EXPECT_TRUE(map.getValWithOffset<int>("t1", index) == 20);
-    EXPECT_TRUE(map.getValWithOffset<int>("t1", index, 3) == 20);
-    EXPECT_TRUE(map.getValWithOffset<int>("t2", index, 3) == 3);
-    delete[] v1;
-}
-
-TEST(TensorMapTest, GetTensorCorrectness)
-{
-    bool* t1_val = new bool(true);
-    float* t2_val = new float[6]{1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f};
-    Tensor t1 = Tensor{MEMORY_CPU, TYPE_BOOL, {1}, t1_val};
-    Tensor t2 = Tensor{MEMORY_CPU, TYPE_FP32, {3, 2}, t2_val};
-
-    int* default_val = new int[4]{0, 1, 2, 3};
-    Tensor default_tensor = Tensor{MEMORY_CPU, TYPE_INT32, {4}, default_val};
-
-    TensorMap map({{"t1", t1}, {"t2", t2}});
-    EXPECT_THROW(map.at("t3"), std::runtime_error);
-    EXPECT_EQUAL_TENSORS(map.at("t1", default_tensor), t1);
-    EXPECT_EQUAL_TENSORS(map.at("t2", default_tensor), t2);
-    EXPECT_EQUAL_TENSORS(map.at("t3", default_tensor), default_tensor);
-    EXPECT_EQUAL_TENSORS(map.at("t3", {}), Tensor());
-
-    delete[] default_val;
-    delete[] t2_val;
-    delete[] t1_val;
-}
-
-TEST(TensorMapTest, GetTensorCorrectnessAtConstTensorMap)
-{
-    bool* t1_val = new bool(true);
-    float* t2_val = new float[6]{1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f};
-    Tensor t1 = Tensor{MEMORY_CPU, TYPE_BOOL, {1}, t1_val};
-    Tensor t2 = Tensor{MEMORY_CPU, TYPE_FP32, {3, 2}, t2_val};
-
-    int* default_val = new int[4]{0, 1, 2, 3};
-    Tensor default_tensor = Tensor{MEMORY_CPU, TYPE_INT32, {4}, default_val};
-
-    const TensorMap map({{"t1", t1}, {"t2", t2}});
-    EXPECT_THROW(map.at("t3"), std::runtime_error);
-    EXPECT_EQUAL_TENSORS(map.at("t1", default_tensor), t1);
-    EXPECT_EQUAL_TENSORS(map.at("t2", default_tensor), t2);
-    EXPECT_EQUAL_TENSORS(map.at("t3", default_tensor), default_tensor);
-    EXPECT_EQUAL_TENSORS(map.at("t3", {}), Tensor());
-
-    delete[] default_val;
-    delete[] t2_val;
-    delete[] t1_val;
-}
-
-TEST(TensorTest, EmptyTensorMinMaxRaiseError)
-{
-    Tensor t1;
-    EXPECT_THROW(t1.min<int>(), std::runtime_error);
-    EXPECT_THROW(t1.max<int>(), std::runtime_error);
-
-    Tensor t2 = Tensor{MEMORY_CPU, TYPE_INT32, {}, nullptr};
-    EXPECT_THROW(t2.min<int>(), std::runtime_error);
-    EXPECT_THROW(t2.max<int>(), std::runtime_error);
-}
-
-using TensorTypes = testing::Types<int8_t, int, float>;
-
-template <typename T>
-class TensorFuncTest : public testing::Test
-{
-};
-
-TYPED_TEST_SUITE(TensorFuncTest, TensorTypes);
-
-TYPED_TEST(TensorFuncTest, MaxCorrectness)
-{
-    using T = TypeParam;
-
-    size_t size = 4;
-
-    T* v1 = new T[size]{T(1), T(2), T(3), T(4)};
-    T* v2 = new T[size]{T(4), T(3), T(2), T(1)};
-    T* v3 = new T[size]{T(1), T(2), T(4), T(3)};
-
-    Tensor t1 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v1);
-    Tensor t2 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v2);
-    Tensor t3 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v3);
-
-    EXPECT_EQ(t1.max<T>(), T(4));
-    EXPECT_EQ(t2.max<T>(), T(4));
-    EXPECT_EQ(t3.max<T>(), T(4));
-
-    delete[] v1;
-    delete[] v2;
-    delete[] v3;
-}
-
-TYPED_TEST(TensorFuncTest, MinCorrectness)
-{
-    using T = TypeParam;
-
-    size_t size = 4;
-
-    T* v1 = new T[size]{T(1), T(2), T(3), T(4)};
-    T* v2 = new T[size]{T(4), T(3), T(2), T(1)};
-    T* v3 = new T[size]{T(1), T(2), T(4), T(3)};
-
-    Tensor t1 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v1);
-    Tensor t2 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v2);
-    Tensor t3 = Tensor(MEMORY_CPU, getTensorType<T>(), {size}, v3);
-
-    EXPECT_EQ(t1.min<T>(), T(1));
-    EXPECT_EQ(t2.min<T>(), T(1));
-    EXPECT_EQ(t3.min<T>(), T(1));
-
-    delete[] v1;
-    delete[] v2;
-    delete[] v3;
-}
-
-TYPED_TEST(TensorFuncTest, AnyCorrectness)
-{
-    using T = TypeParam;
-
-    T* v = new T[4]{T(1), T(2), T(3), T(4)};
-    Tensor t = Tensor{MEMORY_CPU, getTensorType<T>(), {4}, v};
-    EXPECT_TRUE(t.any<T>(T(1)));
-    EXPECT_FALSE(t.any<T>(T(5)));
-    delete[] v;
-}
-
-TYPED_TEST(TensorFuncTest, AllCorrectness)
-{
-    using T = TypeParam;
-
-    constexpr size_t size = 4;
-    T* v1 = new T[size]{T(1), T(1), T(1), T(1)};
-    T* v2 = new T[size]{T(1), T(1), T(1), T(2)};
-    Tensor t1 = Tensor{MEMORY_CPU, getTensorType<T>(), {size}, v1};
-    Tensor t2 = Tensor{MEMORY_CPU, getTensorType<T>(), {size}, v2};
-    EXPECT_TRUE(t1.all<T>(T(1)));
-    EXPECT_FALSE(t2.all<T>(T(2)));
-    delete[] v1;
-    delete[] v2;
-}
-
-TYPED_TEST(TensorFuncTest, SliceCorrectness)
-{
-    using T = TypeParam;
-
-    constexpr int size = 12;
-    T* v = new T[size];
-    for (int i = 0; i < size; ++i)
-    {
-        v[i] = i;
-    }
-
-    DataType dtype = getTensorType<T>();
-    Tensor t1 = Tensor(MEMORY_CPU, dtype, {3, 4}, v);
-    Tensor t2 = t1.slice({2, 4}, 4);
-
-    EXPECT_EQUAL_TENSORS(t2, Tensor(MEMORY_CPU, dtype, {2, 4}, &v[4]));
-    // An overflowed tensor throws an exception.
-    EXPECT_THROW(t1.slice({2, 4}, 5), std::runtime_error);
-
-    delete[] v;
-}
-
-} // end of namespace
+    // Update the value of the tensor and check the new value
+    v1[0] += 1;
+    EXPECT_TRUE(map.getVal<int>("t1
