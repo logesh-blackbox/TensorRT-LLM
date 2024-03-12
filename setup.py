c6ac923cef28173a18607b277a0e97f1c4e9fef8
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:36:24 +0530

    improve

--- setup.py
+++ setup.py
@@ -12,34 +12,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 import platform
+import re
+import sys
+from typing import List
 
-from setuptools import find_packages, setup
+import setuptools
 from setuptools.dist import Distribution
 
-requirements_filename = "requirements-windows.txt" if platform.system(
-) == "Windows" else "requirements.txt"
-with open(requirements_filename) as f:
-    requirements = f.read().splitlines()
 
-    def extract_url(line):
-        return next(filter(lambda x: x[0] != '-', line.split()))
+def extract_url(line: str) -> str:
+    return next(filter(lambda x: x[0] != "-", line.split()))
 
-    extra_URLs = []
+
+def extract_requirements(requirements_filename: str) -> List[str]:
+    with open(requirements_filename) as f:
+        requirements = f.read().splitlines()
+
+    extra_urls = []
     required_deps = []
+
     for line in requirements:
         if line[0] == "#":
             continue
 
-        # handle -i and --extra-index-url options
         if "-i " in line or "--extra-index-url" in line:
-            extra_URLs.append(extract_url(line))
+            extra_urls.append(extract_url(line))
         else:
             required_deps.append(line)
 
+    return required_deps, extra_urls
+
 
 class BinaryDistribution(Distribution):
-
     def has_ext_modules(self):
         return False
 
@@ -47,24 +53,41 @@ class BinaryDistribution(Distribution):
         return True
 
 
-setup_kwargs = {}
+def get_platform_specific_libs() -> List[str]:
+    if sys.platform == "win32":
+        return ["libs/th_common.dll", "libs/nvinfer_plugin_tensorrt_llm.dll"]
+    elif sys.platform.startswith("linux"):
+        return ["libs/libth_common.so", "libs/libnvinfer_plugin_tensorrt_llm.so"]
+    else:
+        raise RuntimeError(f"Unsupported platform: {sys.platform}")
 
-setup(
-    name='tensorrt_llm',
-    version='0.5.0',
-    description='TensorRT-LLM: A TensorRT Toolbox for Large Language Models',
-    install_requires=required_deps,
-    dependency_links=extra_URLs,
-    zip_safe=True,
-    packages=find_packages(),
-    package_data={
-        'tensorrt_llm':
-        (['libs/th_common.dll', 'libs/nvinfer_plugin_tensorrt_llm.dll']
-         if platform.system() == "Windows" else
-         ['libs/libth_common.so', 'libs/libnvinfer_plugin_tensorrt_llm.so']) +
-        ['tools/plugin_gen/templates/*']
-    },
-    python_requires=">=3.7, <4",
-    distclass=BinaryDistribution,
-    **setup_kwargs,
-)
+
+def get_package_data() -> dict:
+    return {
+        "tensorrt_llm": [
+            *get_platform_specific_libs(),
+            "tools/plugin_gen/templates/*"
+        ]
+    }
+
+
+setup_kwargs = {
+    "name": "tensorrt_llm",
+    "version": "0.5.0",
+    "description": "TensorRT-LLM: A TensorRT Toolbox for Large Language Models",
+    "install_requires": [],
+    "dependency_links": [],
+    "zip_safe": True,
+    "packages": setuptools.find_packages(),
+    "package_data": get_package_data(),
+    "python_requires": ">=3.7, <4",
+    "distclass": BinaryDistribution,
+}
+
+required_deps, extra_urls = extract_requirements("requirements.txt")
+setup_kwargs["install_requires"] = required_deps
+setup_kwargs["dependency_links"] = extra_urls
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_kwargs)
+
