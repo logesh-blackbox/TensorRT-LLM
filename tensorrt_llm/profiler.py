b90f9e687d7bde5a3841fb23c139381efb452a69
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:21 +0530

    comment

--- tensorrt_llm/profiler.py
+++ tensorrt_llm/profiler.py
@@ -12,19 +12,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import time
 
-
 class Timer:
+    """
+    This class is used to track the time taken by different sections of the code.
+    It starts a timer when the 'start' method is called with a specific tag,
+    stops the timer when the 'stop' method is called with the same tag,
+    and calculates the elapsed time. The 'elapsed_time_in_sec' method returns
+    the total elapsed time for a specific tag. The 'reset' method clears all
+    start and total elapsed times. The 'summary' method prints the total elapsed
+    time for all tags.
+    """
 
     def __init__(self):
+        """
+        Initializes the Timer object with empty dictionaries for start times and
+        total elapsed times.
+        """
         self._start_times = {}
         self._total_elapsed_times = {}
 
     def start(self, tag):
+        """
+        Starts the timer for the given tag.
+
+        :param tag: The tag to start the timer for.
+        """
         self._start_times[tag] = time.time()
 
     def stop(self, tag) -> float:
+        """
+        Stops the timer for the given tag, calculates the elapsed time, and adds
+        it to the total elapsed time for that tag.
+
+        :param tag: The tag to stop the timer for.
+        :return: The elapsed time for the given tag.
+        """
         elapsed_time = time.time() - self._start_times[tag]
         if tag not in self._total_elapsed_times:
             self._total_elapsed_times[tag] = 0
@@ -32,38 +57,19 @@ class Timer:
         return elapsed_time
 
     def elapsed_time_in_sec(self, tag) -> float:
+        """
+        Returns the total elapsed time in seconds for the given tag.
+
+        :param tag: The tag to get the elapsed time for.
+        :return: The total elapsed time in seconds for the given tag.
+        """
         if tag not in self._total_elapsed_times:
             return None
         return self._total_elapsed_times[tag]
 
     def reset(self):
+        """
+        Resets all start times and total elapsed times.
+        """
         self._start_times.clear()
-        self._total_elapsed_times.clear()
-
-    def summary(self):
-        print('Profile Results')
-        for tag, elapsed_time in self._total_elapsed_times.items():
-            print(f' - {tag.ljust(30, ".")}: {elapsed_time:.6f} (sec)')
-
-
-_default_timer = Timer()
-
-
-def start(tag):
-    _default_timer.start(tag)
-
-
-def stop(tag):
-    return _default_timer.stop(tag)
-
-
-def elapsed_time_in_sec(tag):
-    return _default_timer.elapsed_time_in_sec(tag)
-
-
-def reset():
-    _default_timer.reset()
-
-
-def summary():
-    _default_timer.summary()
+        self._total_elapsed_times.clear
