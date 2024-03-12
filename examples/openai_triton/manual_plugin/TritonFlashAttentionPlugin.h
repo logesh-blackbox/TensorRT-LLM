653a2baf84f6af895aa586ef710184249b92e6ef
logesh-blackbox <148453338+logesh-blackbox@users.noreply.github.com>
2024-03-12 17:34:24 +0530

    comment

--- examples/openai_triton/manual_plugin/TritonFlashAttentionPlugin.h
+++ examples/openai_triton/manual_plugin/TritonFlashAttentionPlugin.h
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #pragma once
 
 #include <NvInferRuntime.h>
@@ -32,82 +33,125 @@ namespace openai_triton::plugin
 class TritonFlashAttentionPlugin : public nvinfer1::IPluginV2DynamicExt
 {
 public:
+    // Constructor for TritonFlashAttentionPlugin
     TritonFlashAttentionPlugin(int numHeads, int headSize, float softmaxScale, nvinfer1::DataType type);
 
+    // Constructor for TritonFlashAttentionPlugin that accepts a serialized buffer
     TritonFlashAttentionPlugin(const void* data, size_t length);
 
+    // Destructor for TritonFlashAttentionPlugin
     ~TritonFlashAttentionPlugin() override = default;
 
     // IPluginV2DynamicExt Methods
+    // Clone the plugin
     nvinfer1::IPluginV2DynamicExt* clone() const noexcept override;
+
+    // Get the output dimensions for a given input
     nvinfer1::DimsExprs getOutputDimensions(int outputIndex, const nvinfer1::DimsExprs* inputs, int nbInputs,
         nvinfer1::IExprBuilder& exprBuilder) noexcept override;
+
+    // Check if the plugin supports a given format combination
     bool supportsFormatCombination(
         int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) noexcept override;
+
+    // Configure the plugin with input and output descriptions
     void configurePlugin(const nvinfer1::DynamicPluginTensorDesc* in, int nbInputs,
         const nvinfer1::DynamicPluginTensorDesc* out, int nbOutputs) noexcept override;
+
+    // Get the workspace size required for the plugin
     size_t getWorkspaceSize(const nvinfer1::PluginTensorDesc* inputs, int nbInputs,
         const nvinfer1::PluginTensorDesc* outputs, int nbOutputs) const noexcept override;
+
+    // Enqueue the plugin for execution
     int enqueue(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
         const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept override;
 
+    // Implementation of enqueue for specific data types
     template <typename T>
     int enqueueImpl(const nvinfer1::PluginTensorDesc* inputDesc, const nvinfer1::PluginTensorDesc* outputDesc,
         const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream);
 
     // IPluginV2Ext Methods
+    // Get the output data type for a given input data type
     nvinfer1::DataType getOutputDataType(
         int index, const nvinfer1::DataType* inputTypes, int nbInputs) const noexcept override;
 
     // IPluginV2 Methods
+    // Get the plugin type
     const char* getPluginType() const noexcept override;
+
+    // Get the plugin version
     const char* getPluginVersion() const noexcept override;
+
+    // Get the number of outputs
     int getNbOutputs() const noexcept override;
+
+    // Initialize the plugin
     int initialize() noexcept override;
+
+    // Terminate the plugin
     void terminate() noexcept override;
+
+    // Get the serialization size of the plugin
     size_t getSerializationSize() const noexcept override;
+
+    // Serialize the plugin
     void serialize(void* buffer) const noexcept override;
+
+    // Destroy the plugin
     void destroy() noexcept override;
+
+    // Set the plugin namespace
     void setPluginNamespace(const char* pluginNamespace) noexcept override;
+
+    // Get the plugin namespace
     const char* getPluginNamespace() const noexcept override;
 
 private:
+    // Layer name
     const std::string mLayerName;
+
+    // Namespace
     std::string mNamespace;
 
+    // Number of heads
     int mNumHeads;
+
+    // Head size
     int mHeadSize;
+
+    // Softmax scale
     float mSoftmaxScale;
+
+    // Data type
     nvinfer1::DataType mType;
 
+    // CUDA module
     CUmodule mModule;
+
+    // CUDA function
     CUfunction mKernel;
 };
 
+// Creator for TritonFlashAttentionPlugin
 class TritonFlashAttentionPluginCreator : public nvinfer1::IPluginCreator
 {
 public:
+    // Constructor for TritonFlashAttentionPluginCreator
     TritonFlashAttentionPluginCreator();
 
+    // Get the plugin name
     const char* getPluginName() const noexcept override;
 
+    // Get the plugin version
     const char* getPluginVersion() const noexcept override;
 
+    // Get the plugin field names
     const nvinfer1::PluginFieldCollection* getFieldNames() noexcept override;
 
+    // Create the plugin
     nvinfer1::IPluginV2* createPlugin(const char* name, const nvinfer1::PluginFieldCollection* fc) noexcept override;
 
+    // Deserialize the plugin
     nvinfer1::IPluginV2* deserializePlugin(
-        const char* name, const void* serialData, size_t serialLength) noexcept override;
-
-    void setPluginNamespace(const char* pluginNamespace) noexcept override;
-
-    const char* getPluginNamespace() const noexcept override;
-
-private:
-    static nvinfer1::PluginFieldCollection mFC;
-    static std::vector<nvinfer1::PluginField> mPluginAttributes;
-    std::string mNamespace;
-};
-
-} // namespace openai_triton::plugin
+       
